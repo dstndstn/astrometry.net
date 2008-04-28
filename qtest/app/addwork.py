4a9dc244ef17128a9348679f0a712a565e81af8a
gmaps alias <dstndstn@gmail.com>
2008-04-28 18:27:22 +0000

    more
    git-svn-id: svn+ssh://astrometry.net/svn/trunk/src/astrometry@7879 03a3cea6-2c03-0410-ac9b-9271b1c66c29

--- qtest/app/addwork.py
+++ qtest/app/addwork.py
@@ -1,5 +1,6 @@
 import os
 os.environ['DJANGO_SETTINGS_MODULE'] = 'qtest.settings'
+import time
 
 from qtest.app.models import *
 
@@ -9,8 +10,16 @@ if __name__ == '__main__':
     ind1 = Index.objects.get(id=1)
 
     # set up Work
+    ids = []
     for i in xrange(10):
         w = Work(index=ind1)
         w.save()
+        ids.append(w.id)
 
+    print 'Added work - waiting for it to be finished.'
+
+    while True:
+        fw = FinishedWork.objects.all().filter(work__id__in=ids)
+        print '%i of %i done' % (fw.count(), len(ids))
+        time.sleep(1)
 
