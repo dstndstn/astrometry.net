8ba13ce7cd4f7065089c2b98486082f43420079c
Dustin Lang <dstndstn@gmail.com>
2008-04-30 20:17:51 +0000

    refactor
    git-svn-id: svn+ssh://astrometry.net/svn/trunk/src/astrometry@7957 03a3cea6-2c03-0410-ac9b-9271b1c66c29

---
+++ blind/resort-xylist-main.c
@@ -0,0 +1,90 @@
+/*
+  This file is part of the Astrometry.net suite.
+  Copyright 2006, 2007 Dustin Lang, Keir Mierle and Sam Roweis.
+
+  The Astrometry.net suite is free software; you can redistribute
+  it and/or modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation, version 2.
+
+  The Astrometry.net suite is distributed in the hope that it will be
+  useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with the Astrometry.net suite ; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+*/
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <assert.h>
+
+#include "qfits.h"
+#include "an-bool.h"
+#include "resort-xylist.h"
+#include "errors.h"
+#include "fitsioutils.h"
+
+const char* OPTIONS = "hdf:b:";
+
+static void printHelp(char* progname) {
+    printf("Usage:   %s  <input> <output>\n"
+		   "      -f <flux-column-name>  (default: FLUX) \n"
+		   "      -b <background-column-name>  (default: BACKGROUND)\n"
+		   "      [-d]: sort in descending order (default is ascending)\n"
+           "\n", progname);
+}
+
+extern char *optarg;
+extern int optind, opterr, optopt;
+
+int main(int argc, char** args) {
+    int argchar;
+	char* infn = NULL;
+	char* outfn = NULL;
+	char* progname = args[0];
+    char* fluxcol = "FLUX";
+    char* backcol = "BACKGROUND";
+    bool ascending = TRUE;
+
+    while ((argchar = getopt (argc, args, OPTIONS)) != -1)
+        switch (argchar) {
+        case 'f':
+            fluxcol = optarg;
+            break;
+        case 'b':
+            backcol = optarg;
+            break;
+        case 'd':
+            ascending = FALSE;
+            break;
+        case '?':
+        case 'h':
+			printHelp(progname);
+            return 0;
+        default:
+            return -1;
+        }
+
+    if (optind != argc-2) {
+        printHelp(progname);
+        exit(-1);
+    }
+
+    infn = args[optind];
+    outfn = args[optind+1];
+
+    fits_use_error_system();
+
+    if (resort_xylist(infn, outfn, fluxcol, backcol, ascending)) {
+        ERROR("Re-sorting xylist by FLUX and BACKGROUND failed");
+        exit(-1);
+    }
+
+    return 0;
+}
