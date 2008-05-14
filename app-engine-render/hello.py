1bf1b5f3e10869dee2cb6871d70331dcb65acde9
Dustin Lang <dstndstn@gmail.com>
2008-05-14 19:51:02 +0000

    switch to wsgiref
    git-svn-id: svn+ssh://astrometry.net/svn/trunk/src/astrometry@8134 03a3cea6-2c03-0410-ac9b-9271b1c66c29

--- app-engine-render/hello.py
+++ app-engine-render/hello.py
@@ -4,20 +4,38 @@ from array import array
 
 import png
 
-print 'Content-Type: image/png'
-print ''
-W = 256
-H = 256
-writer = png.Writer(width=W, height=H, has_alpha=True, compression=1)
-pixels = array('B')
-for x in range(H):
-	for y in range(W):
-		r = x
-		g = y
-		b = 64
-		a = 128
-		pixels.append(r)
-		pixels.append(g)
-		pixels.append(b)
-		pixels.append(a)
-writer.write_array(sys.stdout, pixels)
+import wsgiref.handlers
+from google.appengine.ext import webapp
+
+class MainPage(webapp.RequestHandler):
+	def get(self):
+		res = self.response
+		res.headers['Content-Type'] = 'image/png'
+		W = 256
+		H = 256
+		writer = png.Writer(width=W, height=H, has_alpha=True, compression=1)
+		pixels = array('B')
+		for x in range(H):
+			for y in range(W):
+				r = x
+				g = y
+				b = 64
+				a = 128
+				pixels.append(r)
+				pixels.append(g)
+				pixels.append(b)
+				pixels.append(a)
+		#writer.write_array(sys.stdout, pixels)
+		writer.write_array(res.out, pixels)
+
+		#res.out.write('Hello, webapp World!')
+
+
+
+# yummy boilerplate...
+def main():
+	application = webapp.WSGIApplication([('/', MainPage)], debug=True)
+	wsgiref.handlers.CGIHandler().run(application)
+if __name__ == "__main__":
+	main()
+
