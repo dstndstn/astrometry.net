0c401dbe5df0b7917c74ebe28d7daf55d1c27238
gmaps alias <dstndstn@gmail.com>
2008-05-22 15:33:01 +0000

    more removals
    git-svn-id: svn+ssh://astrometry.net/svn/trunk/src/astrometry@8214 03a3cea6-2c03-0410-ac9b-9271b1c66c29

--- net/server/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('astrometry.net.server.views',
-                       (r'^/?$',        'summary'    ),
-                       (r'^/input/$',   'get_input'  ),
-                       (r'^/results/$', 'set_results'),
-                       (r'^/test', 'test'),
-                       )
