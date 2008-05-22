d38d4517c027dd5244fe48fb418edb8b353ef87a
gmaps alias <dstndstn@gmail.com>
2008-05-22 15:31:06 +0000

    remove complicated distributed server
    git-svn-id: svn+ssh://astrometry.net/svn/trunk/src/astrometry@8213 03a3cea6-2c03-0410-ac9b-9271b1c66c29

--- net/server/run-solver-oven.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-cd /home/gmaps/test/astrometry/net/server
-LOG=worker-$(hostname)-$PPID
-echo "Logging to $LOG.{out,err}"
-touch $LOG.out $LOG.err
-python ./solver.py /home/gmaps/INDEXES/500 >> $LOG.out 2>> $LOG.err
-
