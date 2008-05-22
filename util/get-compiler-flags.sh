58bde4260ba2838e98b358b54300a8a97007ebab
Dustin Lang <dstndstn@gmail.com>
2008-05-22 14:05:23 +0000

    wrestling with compiler flags... [broken]
    git-svn-id: svn+ssh://astrometry.net/svn/trunk/src/astrometry@8205 03a3cea6-2c03-0410-ac9b-9271b1c66c29

--- util/get-compiler-flags.sh
+++ util/get-compiler-flags.sh
@@ -2,65 +2,67 @@
 
 # MacOSX "thinks different" about shared libraries...
 TMPFILE=gcc-out.tmp
-X=`./first-gcc-opt.sh \
-    "$(CC) -x c -o ${TMPFILE} -" \
+X=$(first-gcc-opt.sh \
+    "${CC} -x c -o ${TMPFILE} -" \
     "-shared" \
-    "-dynamic -dynamiclib"`
+    "-dynamic -dynamiclib")
 echo "SHAREDLIBFLAGS_DEF := $X"
 
-X=`./first-gcc-opt.sh \
-    "$(CC) -x c -c -o ${TMPFILE} -" \
-    "-ffinite-math-only -fno-signaling-nans"`
+X=$(first-gcc-opt.sh \
+    "${CC} -x c -c -o ${TMPFILE} -" \
+    "-ffinite-math-only -fno-signaling-nans")
 echo "FLAGS_DEF += $X"
 
+DEFS=$(${CC} -dM -E - < /dev/null)
+
 # use "native" if it's available (gcc 4.2 and above)
-NATIVE="-mtune=native -march=native"
-if `$(CC) -x c -c -o ${TMPFILE} \
-    ${NATIVE} \
-    - < /dev/null > /dev/null 2> /dev/null`; then
-    echo "FLAGS_DEF += ${NATIVE}"
+X=$(first-gcc-opt.sh \
+    "${CC} -x c -c -o ${TMPFILE} -" \
+    "-march=native")
+if [ "x${X}" != "x" ]; then
+    echo "FLAGS_DEF += ${X}";
+
 else
     MACHINE=`uname -m`
 
     if [ "${MACHINE}" = "i686" ]; then
-    # gcc before version 3.1 doesn't support "pentium4"; use "i686" instead.
-    # gcc 3.1: "pentium4"
 
-        X=`./first-gcc-opt.sh \
-            "$(CC) -x c -c -o ${TMPFILE} -" \
+        # gcc before version 3.1 doesn't support "pentium4";
+        #     use "i686" instead.
+        
+        # Note that we're not really justified in assuming this is a P4...
+
+        X=`first-gcc-opt.sh \
+            "${CC} -x c -c -o ${TMPFILE} -" \
             "-march=pentium4" \
             "-march=i686"`
-        echo "FLAGS_DEF += $X"
-    elif [ "${MACHINE}" = "x86_64" ]; then
-    else
-    fi
+        echo "FLAGS_DEF += $X";
 
+    elif [ "${MACHINE}" = "x86_64" ]; then
+
+        K8=$(echo ${DEFS} | grep "\#define __tune_k8__ 1")
+
+        if [ "x${K8}" != "x" ]; then
+            echo "FLAGS_DEF += -march=k8 -m64"
+        fi;
+
+    else
+        echo "FLAGS_DEF += -DNOT_686";
+    fi
 fi
 
-eq ($(MACHINE), i686)
-else
-	K8 := $(shell $(CC) -dM -E - < /dev/null | grep "\#define __tune_k8__ 1")
-	ifneq ($(K8),)
-		FLAGS_DEF += -march=k8 -m64
-	else
-		FLAGS_DEF += -DNOT_686
-		APPLE := $(shell $(CC) -dM -E - < /dev/null | grep __APPLE__)
-		ifneq ($(APPLE),)
-		     FLAGS_DEF += -DNOBOOL
-		endif
-	endif
-endif
+APPLE=$(echo ${DEFS} | grep "__APPLE__")
+if [ "x${APPLE}" != "x" ]; then
+    echo "FLAGS_DEF += -DNOBOOL"
+fi
 
-# FLAGS_DEF are gcc flags that are shared between compiling and
-# linking.  CFLAGS_DEF are compile flags, LDFLAGS_DEG are link flags.
+X=$(first-gcc-opt.sh \
+    "${CC} -x c -c -o ${TMPFILE} -" \
+    "-fno-math-errno -fno-trapping-math")
+echo "FLAGS_DEF += $X"
 
-# speedy!
-#FLAGS_DEF += -O3
-#FLAGS_DEF += -fomit-frame-pointer
-
-TEST_OPTION := -fno-math-errno -fno-trapping-math
-FLAGS_DEF += $(shell $(CC) $(TEST_OPTION) -c -x c -o $(TMPFILE) - < /dev/null > /dev/null 2> /dev/null && echo "$(TEST_OPTION)")
-
-TEST_OPTION := -fno-stack-protector
-FLAGS_DEF += $(shell $(CC) $(TEST_OPTION) -c -x c -o $(TMPFILE) - < /dev/null > /dev/null 2> /dev/null && echo "$(TEST_OPTION)")
+X=$(first-gcc-opt.sh \
+    "${CC} -x c -c -o ${TMPFILE} -" \
+    "-fno-stack-protector")
+echo "FLAGS_DEF += $X"
 
