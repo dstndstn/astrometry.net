cb262545cb50d39cc478d9743623e00ed7f8b15b
Dustin Lang <dstndstn@gmail.com>
2008-05-27 13:24:49 +0000

    convenience
    git-svn-id: svn+ssh://astrometry.net/svn/trunk/src/astrometry@8270 03a3cea6-2c03-0410-ac9b-9271b1c66c29

--- libkd/kdtree_fits_io2.h
+++ libkd/kdtree_fits_io2.h
@@ -47,8 +47,8 @@ int kdtree_fits_append_extras(const kdtree_t* kdtree, const qfits_header* hdr, c
 /**
  Usage patterns:
 
- kdtree_io_t* io = kdtree_io_open("in.kd.fits");
- kdtree_t* kd = kdtree_io_read_tree(io, "mytree");
+ kdtree_fits_t* io = kdtree_fits_open("in.kd.fits");
+ kdtree_t* kd = kdtree_fits_read_tree(io, "mytree");
  // kd contains the tree that was read.
  // io->fb->primheader is the primary header
 
@@ -56,52 +56,67 @@ int kdtree_fits_append_extras(const kdtree_t* kdtree, const qfits_header* hdr, c
  chunk.tablename = "my_extra_data";
  chunk.itemsize = sizeof(int32_t);
  chunk.nrows = kd->ndata;
- kdtree_io_read_chunk(io, &chunk);
+ kdtree_fits_read_chunk(io, &chunk);
 
  // chunk->header
  // chunk->data
 
- kdtree_io_close();
+ kdtree_fits_close();
 
 
 
 
- kdtree_io_t* io = kdtree_io_open_for_writing("out.kd.fits");
+ kdtree_fits_t* io = kdtree_fits_open_for_writing("out.kd.fits");
 
  kdtree_t* mytree = ...;
- kdtree_io_write_tree(io, mytree);
+ kdtree_fits_write_tree(io, mytree);
 
  fitsbin_chunk_t chunk;
  chunk.tablename = "my_extra";
  chunk.data = ...;
  chunk.itemsize = sizeof(int32_t);
  chunk.nrows = mytree->ndata;
- kdtree_io_write_chunk(io, &chunk)
+ kdtree_fits_write_chunk(io, &chunk)
 
- kdtree_io_close();
+ kdtree_fits_close();
 
  */
 
-kdtree_io_t* kdtree_io_open(const char* fn);
+// convenience...
+kdtree_t* kdtree_fits_read(const char* fn, const char* treename,
+                           qfits_header** p_hdr);
 
-kdtree_t* kdtree_io_read_tree(kdtree_io_t* io, const char* treename);
+int kdtree_fits_write(const kdtree_t* kdtree, const char* fn,
+                      const qfits_header* hdr);
 
-int kdtree_io_read_chunk(kdtree_io_t* io, fitsbin_chunk_t* chunk);
+/*
+ FILE* kdtree_fits_write_primary_header(const char* fn);
 
-qfits_header* kdtree_io_get_primary_header(kdtree_io_t* io);
-
-
-
-kdtree_io_t* kdtree_io_open_for_writing(const char* fn);
-
-kdtree_io_write_tree(kdtree_io_t* io, kdtree_t* kd);
-
-fitsbin_t* kdtree_io_get_fitsbin(kdtree_io_t* io);
-
-int kdtree_io_write_chunk(kdtree_io_t* io, fitsbin_chunk_t* chunk);
+ int kdtree_fits_append(const kdtree_t* kdtree, const qfits_header* hdr,
+ FILE* out);
+ */
 
 
-kdtree_io_close(kdtree_io_t* io);
+kdtree_fits_t* kdtree_fits_open(const char* fn);
+
+kdtree_t* kdtree_fits_read_tree(kdtree_fits_t* io, const char* treename);
+
+int kdtree_fits_read_chunk(kdtree_fits_t* io, fitsbin_chunk_t* chunk);
+
+qfits_header* kdtree_fits_get_primary_header(kdtree_fits_t* io);
+
+
+
+kdtree_fits_t* kdtree_fits_open_for_writing(const char* fn);
+
+int kdtree_fits_write_tree(kdtree_fits_t* io, kdtree_t* kd);
+
+fitsbin_t* kdtree_fits_get_fitsbin(kdtree_fits_t* io);
+
+int kdtree_fits_write_chunk(kdtree_fits_t* io, fitsbin_chunk_t* chunk);
+
+
+kdtree_fits_close(kdtree_fits_t* io);
 
 /*
  struct kdtree_io_s {
@@ -111,7 +126,7 @@ kdtree_io_close(kdtree_io_t* io);
  typedef struct kdtree_io_s kdtree_io_t;
  */
 
-typedef fitsbin_t kdtree_io_t;
+typedef fitsbin_t kdtree_fits_t;
 
 /*
  struct extra_table_info {
