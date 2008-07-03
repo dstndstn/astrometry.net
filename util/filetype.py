56399dc5639ec73e821067e6bfb965d25f87b4b5
gmaps alias <dstndstn@gmail.com>
2008-07-03 14:21:40 +0000

    be robust against 'file' output
    git-svn-id: svn+ssh://astrometry.net/svn/trunk/src/astrometry@8558 03a3cea6-2c03-0410-ac9b-9271b1c66c29

--- util/filetype.py
+++ util/filetype.py
@@ -2,6 +2,11 @@ import os
 
 from astrometry.util.shell import shell_escape
 
+# DEBUG
+import logging
+def logverb(*msg):
+    logging.debug(' '.join([str(m).decode('latin_1', 'backslashreplace') for m in msg]))
+
 # Returns a list (usually with just one element) of 2-tuples:
 #  [ (filetype, detail), (filetype, detail) ]
 # eg
@@ -14,9 +19,17 @@ def filetype(fn):
     #logverb('Running: "%s"' % cmd)
     (filein, fileout) = os.popen2(cmd)
     out = fileout.read().strip()
-    #logverb('Result: "%s"' % typeinfo)
 
-    lst = [tuple(line.split(', ', 1)) for line in out.split('\n- ')]
+    logverb('File: "%s"' % out)
+
+    parts = [line.split(', ', 1) for line in out.split('\n- ')]
+    lst = []
+    for p in parts:
+        if len(p) == 2:
+            lst.append(tuple(p))
+        else:
+            lst.append((p[0], ''))
+    #lst = [tuple(line.split(', ', 1)) for line in out.split('\n- ')]
     #logverb('Trimmed: "%s"' % typeinfo)
     return lst
 
