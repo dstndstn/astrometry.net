e7bbc320ebf33319efbc037338d8f49b0920a429
Dustin Lang <dstndstn@gmail.com>
2008-07-13 21:29:55 +0000

    tidy of pyspherematch
    git-svn-id: svn+ssh://astrometry.net/svn/trunk/src/astrometry@8741 03a3cea6-2c03-0410-ac9b-9271b1c66c29

--- libkd/pyspherematch.c
+++ libkd/pyspherematch.c
@@ -1,16 +1,31 @@
+/*
+  This file is part of libkd.
+  Copyright 2008 Dustin Lang.
+
+  libkd is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, version 2.
+
+  libkd is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with libkd; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
 #include "Python.h"
 
 #include <stdio.h>
 
-// PYTHON/site-packages/numpy/core/include
+// numpy - this should be in site-packages/numpy/core/include
 #include "arrayobject.h"
-//#include "ufuncobject.h"
 
 #include "kdtree.h"
 #include "dualtree_rangesearch.h"
 #include "bl.h"
-
-//static void info(PyArrayObject* x);
 
 static PyObject* spherematch_kdtree_build(PyObject* self, PyObject* args) {
     int N, D;
@@ -40,19 +55,6 @@ static PyObject* spherematch_kdtree_build(PyObject* self, PyObject* args) {
         return NULL;
     }
 
-    /*
-     for (i=0; i<N; i++) {
-     printf("data pt = [ ");
-     for (j=0; j<D; j++) {
-     double* pd = PyArray_GETPTR2(x, i, j);
-     double xx = *pd;
-     printf("%g ", xx);
-     }
-     printf("]\n");
-     }
-     info(x);
-     */
-
     data = malloc(N * D * sizeof(double));
     for (i=0; i<N; i++) {
         for (j=0; j<D; j++) {
@@ -65,10 +67,8 @@ static PyObject* spherematch_kdtree_build(PyObject* self, PyObject* args) {
     treetype = KDTT_DOUBLE;
     //treeoptions = KD_BUILD_SPLIT;
     treeoptions = KD_BUILD_BBOX;
-
     kd = kdtree_build(NULL, data, N, D, Nleaf,
                       treetype, treeoptions);
-
     return Py_BuildValue("k", kd);
 }
 
@@ -80,13 +80,10 @@ static PyObject* spherematch_kdtree_free(PyObject* self, PyObject* args) {
         PyErr_SetString(PyExc_ValueError, "need one arg: kdtree identifier (int)");
         return NULL;
     }
-
     // Nasty!
     kd = (kdtree_t*)i;
-
     free(kd->data.any);
     kdtree_free(kd);
-    // return None
     return Py_BuildValue("");
 }
 
@@ -114,19 +111,15 @@ static PyObject* spherematch_match(PyObject* self, PyObject* args) {
         PyErr_SetString(PyExc_ValueError, "need three args: two kdtree identifiers (ints), and search radius");
         return NULL;
     }
-
     // Nasty!
     kd1 = (kdtree_t*)p1;
     kd2 = (kdtree_t*)p2;
 
     dtresults.inds1 = il_new(256);
     dtresults.inds2 = il_new(256);
-
     dualtree_rangesearch(kd1, kd2, 0.0, rad, NULL,
                          callback_dualtree, &dtresults,
                          NULL, NULL);
-
-    //printf("Found %i close pairs.\n", il_size(dtresults.inds1));
 
     N = il_size(dtresults.inds1);
     dims[0] = N;
@@ -143,7 +136,6 @@ static PyObject* spherematch_match(PyObject* self, PyObject* args) {
     il_free(dtresults.inds1);
     il_free(dtresults.inds2);
 
-    // return None
     return PyArray_Return(rtn);
 }
 
@@ -160,31 +152,6 @@ static PyMethodDef spherematchMethods[] = {
 PyMODINIT_FUNC
 initspherematch_c(void) {
     Py_InitModule("spherematch_c", spherematchMethods);
-
     import_array();
 }
-
-/*
-static void info(PyArrayObject* x) {
-    int i;
-    int D;
-    printf("data %p\n", PyArray_DATA(x));
-    //PyArray_BYTES(x);
-    printf("itemsize %i\n", PyArray_ITEMSIZE(x));
-    printf("ndim %i\n", PyArray_NDIM(x));
-    D = PyArray_NDIM(x);
-    //PyArray_DIMS(x);
-    for (i=0; i<D; i++)
-        printf("  dim %i: %li\n", i, PyArray_DIM(x, i));
-    //PyArray_STRIDES(x);
-    for (i=0; i<D; i++)
-        printf("  stride %i: %li\n", i, PyArray_STRIDE(x,i));
-    //PyArray_DESCR(x);
-    //PyArray_BASE(x);
-
- //PyArrayIterObject it;
- //PyArray_ITER_NEXT(it);
- //PyArray_ITER_DATA(it);
-}
- */
 
