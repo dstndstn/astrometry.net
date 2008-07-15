1bc7fae9f06fcf537ce87c704581bfa206501621
gmaps alias <dstndstn@gmail.com>
2008-07-15 22:14:57 +0000

    [dstn] reindent
    git-svn-id: svn+ssh://astrometry.net/svn/trunk/src/astrometry@8803 03a3cea6-2c03-0410-ac9b-9271b1c66c29

--- net/portal/urls.py
+++ net/portal/urls.py
@@ -6,21 +6,21 @@ urlpatterns = (patterns('astrometry.net.portal.newjob',
                         (r'^newlong/$',         'newlong' ),
                         ) +
                patterns('astrometry.net.portal.views',
-					   (r'^status/$',          'jobstatus'),
-					   (r'^getfile/$',         'getfile'  ),
-					   (r'^joblist/$',         'joblist'  ),
-					   (r'^summary/$',         'summary'  ),
-					   (r'^set_description/$', 'job_set_description'),
-					   (r'^substatusxml/$',    'submission_status_xml'),
-					   (r'^changeperms/$',     'changeperms' ),
-					   #(r'^publishtovo/$',    'publishtovo'),
-                       # PLAY
-					   (r'^redgreen$',     'redgreen'    ),
-                       (r'^run-variant/$', 'run_variant' ),
-					   ) +
+                        (r'^status/$',          'jobstatus'),
+                        (r'^getfile/$',         'getfile'  ),
+                        (r'^joblist/$',         'joblist'  ),
+                        (r'^summary/$',         'summary'  ),
+                        (r'^set_description/$', 'job_set_description'),
+                        (r'^substatusxml/$',    'submission_status_xml'),
+                        (r'^changeperms/$',     'changeperms' ),
+                        #(r'^publishtovo/$',    'publishtovo'),
+                        # PLAY
+                        (r'^redgreen$',     'redgreen'    ),
+                        (r'^run-variant/$', 'run_variant' ),
+                        ) +
                patterns('astrometry.net.portal.tags',
-					   (r'^taglist/$',         'taglist'  ),
-					   (r'^add_tag/$',         'job_add_tag' ),
-					   (r'^remove_tag/$',      'job_remove_tag' ),
+                        (r'^taglist/$',         'taglist'  ),
+                        (r'^add_tag/$',         'job_add_tag' ),
+                        (r'^remove_tag/$',      'job_remove_tag' ),
                         )
                )
