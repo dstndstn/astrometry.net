a8c4ef1d6216d24287e657414ff0512b55e78102
gmaps alias <dstndstn@gmail.com>
2008-07-16 01:50:41 +0000

    move unused func to vo.py
    git-svn-id: svn+ssh://astrometry.net/svn/trunk/src/astrometry@8813 03a3cea6-2c03-0410-ac9b-9271b1c66c29

--- net/portal/views.py
+++ net/portal/views.py
@@ -911,39 +911,3 @@ def changeperms(request):
         return HttpResponseRedirect(request.META['HTTP_REFERER'])
     return HttpResponseRedirect(reverse(summary))
 
-@login_required
-@needs_job
-def publishtovo(request):
-    job = request.job
-    if job.submission.user != request.user:
-        return HttpResponse('not your job')
-    if not job.solved():
-        return HttpResponse('job is not solved')
-    wcs = job.tanwcs
-    if not wcs:
-        return HttpResponse('no wcs')
-
-
-    # BIG HACK! - look through LD_LIBRARY_PATH if this is still needed...
-    if not sip.libraryloaded():
-        sip.loadlibrary('/home/gmaps/test/an-common/_sip.so')
-    
-
-    img = voImage(user = request.user,
-                  field = job.field,
-                  image_title = 'Field_%i' % (job.field.id),
-                  instrument = '',
-                  jdate = 0,
-                  wcs = wcs,
-                  )
-    tanwcs = wcs.to_tanwcs()
-    (ra, dec) = tanwcs.pixelxy2radec(wcs.imagew/2, wcs.imageh/2)
-    (img.ra_center, img.dec_center) = (ra, dec)
-    log('tanwcs: ' + str(tanwcs))
-    log('(ra, dec) center: (%f, %f)' % (ra, dec))
-    (ramin, ramax, decmin, decmax) = tanwcs.radec_bounds(10)
-    (img.ra_min, img.ra_max, img.dec_min, img.dec_max) = (ramin, ramax, decmin, decmax)
-    log('Saving vo.Image: ', img)
-    img.save()
-
-    return HttpResponse('Done.')
