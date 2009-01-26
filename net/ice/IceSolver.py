666332a377f48ac5bf60f20660493e458c701eba
gmaps alias <dstndstn@gmail.com>
2009-01-26 16:53:57 +0000

    refactor, and add a locked singleton Ice client class
    git-svn-id: svn+ssh://astrometry.net/svn/trunk/src/astrometry@10396 03a3cea6-2c03-0410-ac9b-9271b1c66c29

--- net/ice/IceSolver.py
+++ net/ice/IceSolver.py
@@ -2,6 +2,7 @@
 
 import sys
 import time
+import threading
 
 import Glacier2
 import Ice
@@ -12,11 +13,6 @@ import SolverIce
 from astrometry.util.file import *
 import astrometry.net.settings as settings
 from astrometry.net.portal.log import log
-
-theice = None
-therouter = None
-thesession = None
-theadapter = None
 
 # FIXME
 configfile = settings.BASEDIR + 'astrometry/net/ice/config.client'
@@ -30,76 +26,123 @@ def logmsg(*msg):
         print ' '.join([str(m) for m in msg])
     else:
         log('IceSolver:', *msg)
-        
-    
 
-def initIce():
-    global theice
-    settings = Ice.InitializationData()
-    settings.properties = Ice.createProperties(None, settings.properties)
-    settings.properties.load(configfile)
-    theice = Ice.initialize(settings)
+theclient = None
+theclientlock = threading.Lock()
 
-def get_ice():
-    if not theice:
-        initIce()
-    return theice
+class SolverClient(object):
+    def __init__(self):
+        pass
 
-def get_router_session(ice):
-    global therouter, thesession
-    if therouter:
-        return (therouter, thesession)
-    router = theice.getDefaultRouter()
-    router = Glacier2.RouterPrx.checkedCast(router)
-    if not router:
-        logmsg('not a glacier2 router')
-        return -1
-    session = None
-    try:
-        session = router.createSession(username, password) #'test-%i' % int(time.time()), 'test')
-    except Glacier2.PermissionDeniedException,ex:
-        logmsg('router session permission denied:', ex)
-    except Glacier2.CannotCreateSessionException,ex:
-        logmsg('router session: cannot create:', ex)
+    def init(self):
+        settings = Ice.InitializationData()
+        settings.properties = Ice.createProperties(None, settings.properties)
+        settings.properties.load(configfile)
+        ice = Ice.initialize(settings)
+        self.ice = ice
 
-    therouter = router
-    thesession = session
+        logmsg('get session')
+        router = ice.getDefaultRouter()
+        router = Glacier2.RouterPrx.checkedCast(router)
+        if not router:
+            logmsg('not a glacier2 router')
+            raise 'not a glacier2 router'
+        session = None
+        try:
+            session = router.createSession(username, password) #'test-%i' % int(time.time()), 'test')
+        except Glacier2.PermissionDeniedException,ex:
+            logmsg('router session permission denied:', ex)
+            raise ex
+        except Glacier2.CannotCreateSessionException,ex:
+            logmsg('router session: cannot create:', ex)
+            raise ex
+        logmsg('router is', router)
+        logmsg('session is', session)
+        self.router = router
+        self.session = session
 
-    return (router, session)
+        logmsg('creating adapter...')
+        self.adapter = ice.createObjectAdapter('Callback.Client')
+        logmsg('created adapter', self.adapter)
+        self.adapter.activate()
 
-def get_adapter(ice, router):
-    global theadapter
-    if theadapter:
-        return theadapter
-    logmsg('creating adapter...')
-    adapter = ice.createObjectAdapter('Callback.Client')
-    #adapter = ice.createObjectAdapter('Callback.Client')
-    #conid = Ice.generateUUID()
-    #adapter = ice.createObjectAdapterWithRouter(conid, router)
-    logmsg('created adapter', adapter)
-    theadapter = adapter
-    return theadapter
 
-def find_all_solvers(ice):
-    q = ice.stringToProxy('SolverIceGrid/Query')
-    q = IceGrid.QueryPrx.checkedCast(q)
-    solvers = q.findAllObjectsByType('::SolverIce::Solver')
-    logmsg('Found %i solvers' % len(solvers))
-    for s in solvers:
-        logmsg('  ', s)
+    # this will be called from multiple threads.
+    def solve(self, jobid, axy, logfunc):
+        category = self.router.getCategoryForClient()
+        myid = Ice.Identity()
+        myid.category = category
+        myid.name = Ice.generateUUID()
+        logmsg('my id:', myid)
 
-    servers = []
-    for s in solvers:
-        logmsg('Resolving ', s)
-        server = SolverIce.SolverPrx.checkedCast(s)
-        servers.append(server)
+        self.adapter.add(LoggerI(logfunc), myid)
+        logproxy = SolverIce.LoggerPrx.uncheckedCast(self.adapter.createProxy(myid))
+        logmsg('my logger:', logproxy)
 
-    return servers
+        logmsg('get servers')
+        servers = self.find_all_solvers()
+        logmsg('servers:', servers)
+
+        results = [SolverResult(s) for s in servers]
+
+        logmsg('making ICE calls...')
+        for (s,r) in zip(servers,results):
+            s.solve_async(r, jobid, axy, logproxy)
+
+        waiting = [r for r in results]
+        tardata = None
+        lastping = time.time()
+        pingperiod = 30 # seconds
+        while len(waiting):
+            time.sleep(1)
+            for r in waiting:
+                if r.isdone():
+                    if r.tardata is not None and r.solved:
+                        tardata = r.tardata
+                        break
+            if tardata:
+                for r in waiting:
+                    if not r.isdone():
+                        logfunc('Cancelling ' + str(r.server))
+                        r.server.cancel(jobid)
+                break
+            waiting = [r for r in waiting if not r.isdone()]
+
+            tnow = time.time()
+            if tnow - lastping > pingperiod:
+                logfunc('Sending pings...')
+                for r in waiting:
+                    r.server.ice_oneway().ice_ping()
+                lastping = tnow
+
+        logmsg('all ICE calls have returned')
+
+        # grace period to let servers send their last log messages.
+        time.sleep(3)
+        return tardata
+
+    def find_all_solvers(self):
+        q = self.ice.stringToProxy('SolverIceGrid/Query')
+        q = IceGrid.QueryPrx.checkedCast(q)
+        solvers = q.findAllObjectsByType('::SolverIce::Solver')
+        logmsg('Found %i solvers' % len(solvers))
+        for s in solvers:
+            logmsg('  ', s)
+        servers = []
+        for s in solvers:
+            logmsg('Resolving ', s)
+            server = SolverIce.SolverPrx.checkedCast(s)
+            servers.append(server)
+        return servers
+
+
+
 
 class LoggerI(SolverIce.Logger):
     def __init__(self, logfunc):
         self.logfunc = logfunc
     def logmessage(self, msg, current=None):
+        #logmsg('logger callback')
         self.logfunc(msg)
 
 class SolverResult(object):
@@ -119,94 +162,15 @@ class SolverResult(object):
         return self.failed or self.tardata is not None
 
 def solve(jobid, axy, logfunc):
-    logmsg('get ice')
-    ice = get_ice()
-    logmsg('ice is', ice)
+    global theclient
+    
+    theclientlock.acquire()
+    if not theclient:
+        theclient = SolverClient()
+        theclient.init()
+    theclientlock.release()
 
-    logmsg('get session')
-    (router, session) = get_router_session(ice)
-    logmsg('router is', router)
-    logmsg('session is', session)
-
-    #conid = Ice.generateUUID()
-    #logmsg('conid:', conid)
-    #router.ice_connectionId(conid)
-
-    logmsg('get servers')
-    servers = find_all_solvers(ice)
-    logmsg('servers:', servers)
-
-    #props = ice.getProperties()
-    # Or possibly createObjectAdapterWithRouter, see 43.4.4
-    #adapter = ice.createObjectAdapterWithRouter('Callback.Client', router)
-
-    logmsg('get adapter...')
-    adapter = get_adapter()
-    logmsg('adapter is', adapter)
-
-    category = router.getCategoryForClient()
-    #myid = ice.stringToIdentity('callbackReceiver')
-    myid = Ice.Identity()
-    myid.category = category
-    myid.name = Ice.generateUUID()
-    logmsg('my id:', myid)
-
-    adapter.add(LoggerI(logfunc), myid)
-    adapter.activate()
-
-    logproxy = SolverIce.LoggerPrx.uncheckedCast(adapter.createProxy(myid))
-
-    logmsg('my category:', category)
-    logmsg('my adapter:', adapter)
-    logmsg('my logger:', logproxy)
-
-    results = [SolverResult(s) for s in servers]
-
-    logmsg('making ICE calls...')
-    for (s,r) in zip(servers,results):
-        s.solve_async(r, jobid, axy, logproxy)
-
-    waiting = [r for r in results]
-    tardata = None
-
-    lastping = time.time()
-    pingperiod = 30 # seconds
-
-    while len(waiting):
-        time.sleep(1)
-        for r in waiting:
-            if r.isdone():
-                if r.tardata is not None and r.solved:
-                    tardata = r.tardata
-                    break
-        if tardata:
-            for r in waiting:
-                if not r.isdone():
-                    logfunc('Cancelling ' + str(r.server))
-                    r.server.cancel(jobid)
-            break
-        waiting = [r for r in waiting if not r.isdone()]
-
-        tnow = time.time()
-        if tnow - lastping > pingperiod:
-            logfunc('Sending pings...')
-            for r in waiting:
-                r.server.ice_oneway().ice_ping()
-            lastping = tnow
-
-    logmsg('all ICE calls have returned')
-
-    # grace period to let servers send their last log messages.
-    time.sleep(3)
-
-    try:
-        logmsg('IceSolver: destroying session')
-        router.destroySession()
-    except Glacier2.SessionNotExistException, ex:
-        logmsg('Destroying session:', ex)
-    except Ice.ConnectionLostException:
-        # Expected: the router closed the connection.
-        pass
+    tardata = theclient.solve(jobid, axy, logfunc)
 
     return tardata
 
