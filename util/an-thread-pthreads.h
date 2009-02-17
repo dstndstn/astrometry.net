d4a7d5ef5ceb00314f89528f210ff937121404c0
Dustin Lang <dstndstn@gmail.com>
2009-02-17 15:33:28 +0000

    thread debug
    git-svn-id: svn+ssh://astrometry.net/svn/trunk/src/astrometry@10584 03a3cea6-2c03-0410-ac9b-9271b1c66c29

--- util/an-thread-pthreads.h
+++ util/an-thread-pthreads.h
@@ -29,9 +29,29 @@
 
 #define AN_THREAD_DECLARE_MUTEX(X) pthread_mutex_t X = PTHREAD_MUTEX_INITIALIZER
 
-#define AN_THREAD_DECLARE_STATIC_MUTEX(X) static pthread_mutex_t X = PTHREAD_MUTEX_INITIALIZER
+//#define AN_THREAD_DECLARE_STATIC_MUTEX(X) static pthread_mutex_t X = PTHREAD_MUTEX_INITIALIZER
+#define AN_THREAD_DECLARE_STATIC_MUTEX(X) \
+    static pthread_mutex_t X; \
+static inline void init_mutex_ ## X() { \
+    pthread_mutexattr_t attr; \
+    pthread_mutexattr_init(&attr); \
+    pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_ERRORCHECK); \
+    pthread_mutex_init(&X, &attr); \
+    pthread_mutexattr_destroy(&attr); \
+} \
+    static pthread_once_t X_once = PTHREAD_ONCE_INIT
 
-#define AN_THREAD_LOCK(X) pthread_mutex_lock(&X)
+
+//#define AN_THREAD_LOCK(X) pthread_mutex_lock(&X)
+#define AN_THREAD_LOCK(X) { \
+    pthread_once(&X_once, init_mutex_ ## X); \
+    pthread_mutex_lock(&X); \
+}
+/*
+//inline AN_THREAD_LOCK(pthread_mutex_t* m) {
+printf("Locking mutex %p...\n", m);
+}
+*/
 
 #define AN_THREAD_UNLOCK(X) pthread_mutex_unlock(&X)
 
