8569f6529f6b31c2a5fb16a98e6632e3c1d21e13
Dustin Lang <dstndstn@gmail.com>
2009-03-09 12:02:31 +0000

    render grid: just mask once
    git-svn-id: svn+ssh://astrometry.net/svn/trunk/src/astrometry@10803 03a3cea6-2c03-0410-ac9b-9271b1c66c29

--- render/render_gridlines.c
+++ render/render_gridlines.c
@@ -33,11 +33,13 @@ int render_gridlines(cairo_t* c2, render_args_t* args) {
 	 In order to properly do the transparency and text, we render onto a
 	 mask image, then squish paint through this mask onto the given image.
 	 */
-	target = cairo_image_surface_create(CAIRO_FORMAT_A8, args->W, args->H);
-	cairo = cairo_create(target);
+	//target = cairo_image_surface_create(CAIRO_FORMAT_A8, args->W, args->H);
+	target = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, args->W, args->H);
+   	cairo = cairo_create(target);
 	cairo_set_line_width(cairo, 1.0);
 	cairo_set_antialias(cairo, CAIRO_ANTIALIAS_GRAY);
-	cairo_set_source_rgb(cairo, 1.0, 1.0, 1.0);
+	//cairo_set_source_rgb(cairo, 1.0, 1.0, 1.0);
+	cairo_set_source_rgba(cairo, 1.0, 1.0, 1.0, 0.6);
 	cairo_select_font_face(cairo, "DejaVu Sans Mono Book", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_BOLD);
 	//cairo_set_font_size(cairo, 16);
 	cairo_set_font_size(cairo, 14);
@@ -68,11 +70,13 @@ int render_gridlines(cairo_t* c2, render_args_t* args) {
 	}
 	cairo_stroke(cairo);
 	
-	cairo_set_source_rgba(c2, 0.8, 0.8, 1.0, 0.6);
-	cairo_mask_surface(c2, target, 0, 0);
-	cairo_select_font_face(c2, "DejaVu Sans Mono Book", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_BOLD);
-	cairo_set_font_size(c2, 16);
-	cairo_set_source_rgba(c2, 0.8, 0.8, 1.0, 1.0);
+	cairo_set_source_rgba(cairo, 1.0, 1.0, 1.0, 1.0);
+
+	/*
+	 cairo_select_font_face(c2, "DejaVu Sans Mono Book", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_BOLD);
+	 cairo_set_font_size(c2, 16);
+	 cairo_set_source_rgba(c2, 0.8, 0.8, 1.0, 1.0);
+	 */
 	for (ra = ralabelstep * floor(args->ramin / ralabelstep);
 		 ra <= ralabelstep * ceil(args->ramax / ralabelstep);
 		 ra += ralabelstep) {
@@ -81,9 +85,9 @@ int render_gridlines(cairo_t* c2, render_args_t* args) {
 		if (!in_image((int)round(x+0.5), 0, args))
 			continue;
 		sprintf(buf, "%i", (int)ra);
-		cairo_text_extents(c2, buf, &ext);
-		cairo_move_to(c2, x - (ext.width - ext.x_bearing)/2, args->H - (ext.height + ext.y_bearing) - textmargin);
-		cairo_show_text(c2, buf);
+		cairo_text_extents(cairo, buf, &ext);
+		cairo_move_to(cairo, x - (ext.width - ext.x_bearing)/2, args->H - (ext.height + ext.y_bearing) - textmargin);
+		cairo_show_text(cairo, buf);
 	}
 	for (dec = declabelstep * floor(args->decmin / declabelstep);
 		 dec <= declabelstep * ceil(args->decmax / declabelstep);
@@ -96,10 +100,13 @@ int render_gridlines(cairo_t* c2, render_args_t* args) {
 		if (!in_image(0, (int)round(y+0.5), args))
 			continue;
 		sprintf(buf, "%i", (int)dec);
-		cairo_text_extents(c2, buf, &ext);
-		cairo_move_to(c2, textmargin, y - ext.y_bearing/2.0);
-		cairo_show_text(c2, buf);
+		cairo_text_extents(cairo, buf, &ext);
+		cairo_move_to(cairo, textmargin, y - ext.y_bearing/2.0);
+		cairo_show_text(cairo, buf);
 	}
+
+	cairo_set_source_rgba(c2, 0.8, 0.8, 1.0, 0.8);
+	cairo_mask_surface(c2, target, 0, 0);
 
 	cairo_surface_destroy(target);
 	cairo_destroy(cairo);
