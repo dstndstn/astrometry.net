b39e4e08bbf236c86881fcfd5557ca0d31fc1d09
Dustin Lang <dstndstn@gmail.com>
2009-03-16 14:52:35 +0000

    fix-sdss-idr: passthru non-idR files
    git-svn-id: svn+ssh://astrometry.net/svn/trunk/src/astrometry@10847 03a3cea6-2c03-0410-ac9b-9271b1c66c29

--- util/fix-sdss-idr.py
+++ util/fix-sdss-idr.py
@@ -1,68 +1,63 @@
+import sys
 import pyfits
 
-# Takes a pyfits object and modifies it.
-def fix_sdss_idr(fits):
-	hdr = fits[0].header
-	if hdr['SIMPLE'] is False:
-		hdr.update('SIMPLE', True, 'FITS compliant (fix-sdss-idr.py)')
+# Takes a pyfits HDU object (which is unchanged) and returns a new
+# pyfits object containing the fixed file.
+def fix_sdss_idr(hdu):
+	hdr = hdu.header.copy()
+
+	if hdr.get('SIMPLE', True):
+		print 'SIMPLE = T: not an SDSS idR file.'
+		return hdu
+	print 'Setting SIMPLE = True'
+	hdr.update('SIMPLE', True, 'FITS compliant (via fix-sdss-idr.py)')
 
 	if hdr.get('SDSS', None) == pyfits.UNDEFINED:
+		print 'Setting SDSS = True'
 		hdr['SDSS'] = True
-		print 'Set SDSS to True'
-
-	print 'UNSIGNED in hdr?', ('UNSIGNED' in hdr)
-	print hdr.get('UNSIGNED', None)
-
-	print hdr.ascardlist()[6]
-	print type(hdr.ascardlist()[6])
-	print hdr.ascardlist()[6].key
-	print hdr.ascardlist()[6].value
-	print hdr.ascardlist()[6].comment
-	print hdr[6]
-
-	if hdr.get('UNSIGNED', None) == pyfits.UNDEFINED:
+	else:
+		print 'No SDSS header card: not an SDSS idR file.'
+		return hdu
+		
+	if (hdr.get('UNSIGNED', None) == pyfits.UNDEFINED
+		and (not 'BSCALE' in hdr) and (not 'BZERO' in hdr)):
+		print 'Setting UNSIGNED = True'
 		hdr['UNSIGNED'] = True
-		print 'Set UNSIGNED to True'
-		if (not 'BSCALE' in hdr) and (not 'BZERO' in hdr):
-			print 'Adding BZERO = 32768 card'
-			hdr.update('BZERO', 32768, 'Unsigned -> signed')
+		print 'Adding BZERO = 32768 card'
+		hdr.update('BZERO', 32768, 'Unsigned -> signed')
+	else:
+		print 'No UNSIGNED header card, or unexpected BSCALE or BZERO card: not an SDSS idR file.'
+		return hdu
 
-	# ?? What if it *doesn't* contain SDSS and UNSIGNED ??
-
-	print 'type:', type(fits[0])
-	print 'data type:', type(fits[0].data)
-	print 'hdu type:', fits[0].header._hdutype
-	print 'card 0:', fits[0].header.ascard[0]
-
-	fits[0].header._updateHDUtype()
-
-	print 'hdu type:', fits[0].header._hdutype
-	data = fits[0].data
-	print 'data type:', type(data)
-	print 'data:', data
-
-	hdr = fits[0].header
-	newhdu = hdr._hdutype(data=data, header=hdr)
+	hdr._updateHDUtype()
+	newhdu = hdr._hdutype(data=pyfits.DELAYED, header=hdr)
 	print 'newhdu:', newhdu
-	newhdu._file = fits[0]._file
-	newhdu._ffile = fits[0]._ffile
-	newhdu._datLoc = fits[0]._datLoc
+	## HACK - encapsulation violation
+	newhdu._file = hdu._file
+	newhdu._ffile = hdu._ffile
+	newhdu._datLoc = hdu._datLoc
 	newhdu.__getattr__('data')
 	print 'data:', type(newhdu.data)
 	print 'data:', newhdu.data.dtype
 	print 'data range:', newhdu.data.min(), 'to', newhdu.data.max()
+	return newhdu
 
-	newhdu.writeto('out.fits', clobber=True)
-
-# _File._readHDU
-#  -> _TempHDU()
-#  -> HDUList.__getitem__
-#    -> _TempHDU.setupHDU()
-#       -> Header(...)
-#         -> sets header._hdutype
-#       -> header._hdutype(data=...)
-
-# SIMPLE=F -> _ValidHDU
+	#newhdu2 = pyfits.PrimaryHDU(data=pyfits.DELAYED, header=hdr)
+	#newhdu2 = newhdu2.setupHDU()
+	#print 'newhdu2:', newhdu2
+	#print 'data:', type(newhdu2.data)
+	#print 'data:', newhdu2.data.dtype
+	#print 'data range:', newhdu2.data.min(), 'to', newhdu2.data.max()
 
 
-X = fix_sdss_idr(pyfits.open('idR-002987-r3-0059.fit'))
+
+if __name__ == '__main__':
+	if len(sys.argv) != 3:
+		print 'Usage: %s <input-fits-file> <output-fits-file>' % sys.argv[0]
+		sys.exit(-1)
+	infile = sys.argv[1]
+	outfile = sys.argv[2]
+
+	newhdu = fix_sdss_idr(pyfits.open(infile)[0])
+	newhdu.writeto(outfile, clobber=True)
+
