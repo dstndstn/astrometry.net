30eeb4e08cd7fe03bb888b3e98720b96e2248840
Dustin Lang <dstndstn@gmail.com>
2009-04-23 18:15:54 +0000

    git-svn-id: svn+ssh://astrometry.net/svn/trunk/src/astrometry@11345 03a3cea6-2c03-0410-ac9b-9271b1c66c29

--- util/pyfits_utils.py
+++ util/pyfits_utils.py
@@ -1,9 +1,20 @@
 import pyfits
 
+class tabledata(object):
+	def __setattr__(self, name, val):
+		object.__setattr__(self, name, val)
+	def set(self, name,val):
+		self.__setattr__(name, val)
+
 def table_fields(data):
 	colnames = data.dtype.names
 	#fields = {}
-	fields = object()
+	#fields = object()
+	fields = tabledata()
 	for c in colnames:
-		fields[c.tolower()] = data.field(c)
+		#print 'Adding field', c.lower()
+		#fields[c.lower()] = data.field(c)
+		#fields.__dict__[c.lower()] = data.field(c)
+		#object.__setattr__(fields, c.lower(), data.field(c))
+		fields.set(c.lower(), data.field(c))
 	return fields
