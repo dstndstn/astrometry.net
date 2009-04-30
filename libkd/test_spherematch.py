cfd2fb74c7c92bdd29449a447cdf55bd2a2777e4
Dustin Lang <dstndstn@gmail.com>
2009-04-30 13:15:21 +0000

    test_spherematch: test dists
    git-svn-id: svn+ssh://astrometry.net/svn/trunk/src/astrometry@11413 03a3cea6-2c03-0410-ac9b-9271b1c66c29

--- libkd/test_spherematch.py
+++ libkd/test_spherematch.py
@@ -15,29 +15,33 @@ t0 = time()
 (inds,dists) = spherematch.match(x1, x2, r)
 dt = time() - t0
 
-print 'C     : found', len(inds), 'pairs in', int(dt*1000.), 'ms'
+print 'spherematch.match: found', len(inds), 'pairs in', int(dt*1000.), 'ms'
 
 order = argsort(inds[:,0]*N2 + inds[:,1])
 inds = inds[order]
+dists = dists[order]
 
 t0 = time()
 pairs = []
+truedists = []
 for i in range(N1):
-    pt1 = x1[i,:]
-    d2s = sum((x2 - pt1)**2, axis=1)
-    good = where(d2s <= r**2)[0]
-    for j in good:
-        pairs.append((i, j))
-pairs = array(pairs)
+	pt1 = x1[i,:]
+	d2s = sum((x2 - pt1)**2, axis=1)
+	good = where(d2s <= r**2)[0]
+	for j in good:
+		pairs.append((i, j))
+		truedists.append(d2s[j])
 dt = time() - t0
+pairs = array(pairs)
+truedists = sqrt(array(truedists))
 
-print 'Python: found', len(pairs), 'pairs in', int(dt*1000.), 'ms'
+print 'naive			: found', len(pairs), 'pairs in', int(dt*1000.), 'ms'
 
 order = argsort(pairs[:,0]*N2 + pairs[:,1])
 pairs = pairs[order]
 
-#print pairs
-
 ok = array_equal(pairs, inds)
-print 'Equal:', ok
+print 'Indices equal:', ok
 
+ok = array_equal(truedists[order], dists.ravel())
+print 'Dists equal:', ok
