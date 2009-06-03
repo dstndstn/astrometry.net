b74714e811f3592f568921c60297d4cebe49c7fa
Dustin Lang <dstndstn@gmail.com>
2009-06-03 15:53:45 +0000

    table_field.getcolumn()
    git-svn-id: svn+ssh://astrometry.net/svn/trunk/src/astrometry@11769 03a3cea6-2c03-0410-ac9b-9271b1c66c29

--- util/pyfits_utils.py
+++ util/pyfits_utils.py
@@ -6,7 +6,7 @@ class tabledata(object):
 	def set(self, name,val):
 		self.__setattr__(name, val)
 	def getcolumn(self, name):
-		return self.__getattr__(name)
+		return self.__dict__[name]
 
 def table_fields(dataorfn):
 	pf = None
