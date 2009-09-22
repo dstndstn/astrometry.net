504fea6dd90b0a9ed150f4a9230428f25f825d71
Dustin Lang <dstndstn@gmail.com>
2009-09-22 03:39:31 +0000

    add NGC/IC fits files
    git-svn-id: svn+ssh://astrometry.net/svn/trunk/src/astrometry@12974 03a3cea6-2c03-0410-ac9b-9271b1c66c29

--- blind/ngc2000tofits.py
+++ blind/ngc2000tofits.py
@@ -4,13 +4,28 @@ from ngc2000 import ngc2000, ngc2000accurate
 
 if __name__ == '__main__':
 
-	ra  = array([x['ra'] for x in ngc2000])
-	dec = array([x['dec'] for x in ngc2000])
-	radius = array([x['size'] for x in ngc2000])
-	ngcnum = array([x['id'] for x in ngc2000])
+	# convert to a form that leads to simple updating with the "accurate" versions.
+	nummap = dict([[(x['is_ngc'], x['id']), (x['ra'],x['dec'],x['size'])]
+				   for x in ngc2000])
+	print 'got %i' % len(nummap)
+
+	# update with "accurate" values.
+	nup = 0
+	for x in ngc2000accurate:
+		key = (x['is_ngc'], x['id'])
+		if key in nummap:
+			(oldra, olddec, size) = nummap[key]
+			nummap[key] = (x['ra'], x['dec'], size)
+			nup +=1
+	print 'updated %i' % nup
+
+	isngc  = array([i for (i,n) in nummap.iterkeys()])
+	ngcnum = array([n for (i,n) in nummap.iterkeys()])
+	ra     = array([r for (r,d,s) in nummap.itervalues()])
+	dec    = array([d for (r,d,s) in nummap.itervalues()])
+	radius = array([s for (r,d,s) in nummap.itervalues()])
 	# turn from diameter in arcmin to radius in deg.
 	radius /= (2. * 60.)
-	isngc = array([x['is_ngc'] for x in ngc2000])
 
 	print 'got %i RA (%i ngc)' % (len(ra), sum(isngc))
 
@@ -18,10 +33,10 @@ if __name__ == '__main__':
 	#phdr = prim.header
 	#phdr.update('AN_FILE', 'RDLS', 'Astrometry.net RA,Dec list')
 	table = pyfits.new_table(
-		[pyfits.Column(name='NGCNUM', format='1D', array=ngcnum[isngc], unit=''),
-		 pyfits.Column(name='RA', format='1D', array=ra[isngc], unit='deg'),
-		 pyfits.Column(name='DEC', format='1D', array=dec[isngc], unit='deg'),
-		 pyfits.Column(name='RADIUS', format='1D', array=radius[isngc], unit='deg'),
+		[pyfits.Column(name='NGCNUM', format='1I', array=ngcnum[isngc], unit=''),
+		 pyfits.Column(name='RA', format='1E', array=ra[isngc], unit='deg'),
+		 pyfits.Column(name='DEC', format='1E', array=dec[isngc], unit='deg'),
+		 pyfits.Column(name='RADIUS', format='1E', array=radius[isngc], unit='deg'),
 		 ])
 	pyfits.HDUList([prim, table]).writeto('ngc.fits', clobber=True)
 
@@ -31,9 +46,9 @@ if __name__ == '__main__':
 	#phdr = prim.header
 	#phdr.update('AN_FILE', 'RDLS', 'Astrometry.net RA,Dec list')
 	table = pyfits.new_table(
-		[pyfits.Column(name='ICNUM', format='1D', array=ngcnum[isic], unit=''),
-		 pyfits.Column(name='RA', format='1D', array=ra[isic], unit='deg'),
-		 pyfits.Column(name='DEC', format='1D', array=dec[isic], unit='deg'),
-		 pyfits.Column(name='RADIUS', format='1D', array=radius[isic], unit='deg'),
+		[pyfits.Column(name='ICNUM', format='1I', array=ngcnum[isic], unit=''),
+		 pyfits.Column(name='RA', format='1E', array=ra[isic], unit='deg'),
+		 pyfits.Column(name='DEC', format='1E', array=dec[isic], unit='deg'),
+		 pyfits.Column(name='RADIUS', format='1E', array=radius[isic], unit='deg'),
 		 ])
 	pyfits.HDUList([prim, table]).writeto('ic.fits', clobber=True)
