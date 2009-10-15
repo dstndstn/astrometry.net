8828105c86c731400d22217f8e3ee7008cfbf76e
Dustin Lang <dstndstn@gmail.com>
2009-10-15 17:20:44 +0000

    oops, add qfits_convert.{c,h}
    git-svn-id: svn+ssh://astrometry.net/svn/trunk/src/astrometry@13277 03a3cea6-2c03-0410-ac9b-9271b1c66c29

---
+++ qfits-an/src/qfits_convert.c
@@ -0,0 +1,124 @@
+
+/** Copyright 2009 Dustin Lang.
+ */
+
+#include <stdint.h>
+#include <sys/param.h>
+
+#include "config.h"
+#include "qfits_image.h"
+#include "qfits_byteswap.h"
+#include "qfits_error.h"
+
+#define FITSOUTPUTPIXEL(fitstype, ival, optr)	\
+	do {										\
+		uint8_t* o8;							\
+		int16_t* o16;							\
+		int32_t* o32;							\
+		float*   ofloat;						\
+		double*  odouble;						\
+		switch (fitstype) {						\
+		case BPP_8_UNSIGNED:					\
+			o8 = optr;							\
+			*o8 = MIN(255, MAX(0, ival));		\
+			break;								\
+		case BPP_16_SIGNED:								 \
+			o16 = optr;									 \
+			*o16 = MIN(INT16_MAX, MAX(INT16_MIN, ival));	\
+			break;											\
+		case BPP_32_SIGNED:									\
+			o32 = optr;										\
+			*o32 = MIN(INT32_MAX, MAX(INT32_MIN, ival));	\
+			break;											\
+		case BPP_IEEE_FLOAT:								\
+			ofloat = optr;									\
+			*ofloat = ival;									\
+			break;											\
+		case BPP_IEEE_DOUBLE:								\
+			odouble = optr;									\
+			*odouble = ival;								\
+			break;												\
+		default:													\
+			qfits_error("Unknown output FITS type %i\n", fitstype); \
+			return -1;												\
+		}															\
+	} while (0)
+
+int qfits_pixel_ctype_size(int ctype) {
+	switch (ctype) {
+	case PTYPE_DOUBLE:
+		return sizeof(double);
+	case PTYPE_FLOAT:
+		return sizeof(float);
+	case PTYPE_INT:
+		return sizeof(int);
+	case PTYPE_UINT8:
+		return 1;
+	}
+	return -1;
+}
+
+int qfits_pixel_fitstype_size(int fitstype) {
+	int n = BYTESPERPIXEL(fitstype);
+	if (!n)
+		return -1;
+	return n;
+}
+
+int qfits_pixel_ctofits(int ctype, int fitstype,
+						const void* cval, void* fitsval) {
+	const float* ifloat;
+	const double* idouble;
+	const int* iint;
+	const uint8_t* iu8;
+
+	switch (ctype) {
+	case PTYPE_DOUBLE:
+		idouble = cval;
+		FITSOUTPUTPIXEL(fitstype, *idouble, fitsval);
+		break;
+
+	case PTYPE_FLOAT:
+		ifloat = cval;
+		FITSOUTPUTPIXEL(fitstype, *ifloat, fitsval);
+		break;
+
+	case PTYPE_INT:
+		iint = cval;
+		FITSOUTPUTPIXEL(fitstype, *iint, fitsval);
+		break;
+
+	case PTYPE_UINT8:
+		iu8 = cval;
+		// This generates warnings about "comparison always false due to limited range of data".
+		// These warnings are harmless.
+		FITSOUTPUTPIXEL(fitstype, *iu8, fitsval);
+		break;
+
+	default:
+		return -1;
+	}
+
+	// Byteswap, if necessary.
+#ifndef WORDS_BIGENDIAN
+	switch (fitstype) {
+	case BPP_8_UNSIGNED:
+		break;
+	case BPP_16_SIGNED:
+		qfits_swap_bytes(fitsval, 2);
+		break;
+	case BPP_32_SIGNED:
+	case BPP_IEEE_FLOAT:
+		qfits_swap_bytes(fitsval, 4);
+		break;
+	case BPP_IEEE_DOUBLE:
+		qfits_swap_bytes(fitsval, 8);
+		break;
+	}
+#endif
+	return 0;
+}
+
+
+#undef FITSOUTPUTPIXEL
+
---
+++ qfits-an/src/qfits_convert.h
@@ -0,0 +1,28 @@
+
+/** Copyright 2009 Dustin Lang.
+ */
+
+/**
+ Converts a single pixel from C to FITS format.
+
+ "ctype" = PTYPE_FLOAT, PTYPE_INT, etc.
+ "fitstype" = BPP_8_UNSIGNED, BPP_16_SIGNED, etc.
+
+ "cval": pointer to C value (input)
+ "fitsval": pointer to FITS value (output)
+
+Does byte-swapping, if necessary.
+ */
+int qfits_pixel_ctofits(int ctype, int fitstype,
+						const void* cval, void* fitsval);
+
+
+/**
+ Returns the size in bytes of the given C pixel type.
+ */
+int qfits_pixel_ctype_size(int ctype);
+
+/**
+ Returns the size in bytes of the given FITS pixel type.
+ */
+int qfits_pixel_fitstype_size(int fitstype);
