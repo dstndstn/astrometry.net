ff660e7b2746c566c8756eb415521cb6663be2ab
Dustin Lang <dstndstn@gmail.com>
2009-10-17 12:58:20 +0000

    update README with Mac fink instructions
    git-svn-id: svn+ssh://astrometry.net/svn/trunk/src/astrometry@13304 03a3cea6-2c03-0410-ac9b-9271b1c66c29

--- util/defaults.py
+++ util/defaults.py
@@ -6,7 +6,7 @@ from matplotlib import rc
 #rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
 rc('font', family='computer modern roman')
 rc('text', usetex=True)
-rc('text.latex', preamble=r'\usepackage{color}')
+#rc('text.latex', preamble=r'\usepackage{color}')
 rc('lines', antialiased=True)
 rc('legend', fontsize='medium', numpoints=1) # default: large
 rc('axes', titlesize='medium')
