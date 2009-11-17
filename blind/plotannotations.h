e768dba13dc4fc14885ef73a4fd6d437865cdea3
Dustin Lang <dstndstn@gmail.com>
2009-11-17 18:46:57 +0000

    add missing file
    git-svn-id: svn+ssh://astrometry.net/svn/trunk/src/astrometry@13657 03a3cea6-2c03-0410-ac9b-9271b1c66c29

---
+++ blind/plotannotations.h
@@ -0,0 +1,13 @@
+#ifndef PLOTANNOTATIONS_H
+#define PLOTANNOTATIONS_H
+
+#include "plotstuff.h"
+
+void* plot_annotations_init(plot_args_t* args);
+
+int plot_annotations_command(const char* command, cairo_t* cr,
+					plot_args_t* args, void* baton);
+
+void plot_annotations_free(plot_args_t* args, void* baton);
+
+#endif
