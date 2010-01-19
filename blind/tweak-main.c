b868fb20c1109478cf840b442ae28a148a83aecc
Dustin Lang <dstndstn@gmail.com>
2010-01-19 20:32:52 +0000

    start implementing log(scale) vs rotation histogram cross-correlation trick
    git-svn-id: svn+ssh://astrometry.net/svn/trunk/src/astrometry@14018 03a3cea6-2c03-0410-ac9b-9271b1c66c29

--- blind/tweak-main.c
+++ blind/tweak-main.c
@@ -23,6 +23,19 @@
 #include <assert.h>
 #include <sys/param.h>
 
+/**
+
+ wget "http://antwrp.gsfc.nasa.gov/apod/image/0403/cmsky_cortner_full.jpg"
+ solve-field --backend-config backend.cfg -v --keep-xylist %s.xy --continue --scale-low 10 --scale-units degwidth cmsky_cortner_full.xy --no-tweak
+ cp cmsky_cortner_full.xy 1.xy
+ cp cmsky_cortner_full.rdls 1.rd
+ cp cmsky_cortner_full.wcs 1.wcs
+ cp cmsky_cortner_full.jpg 1.jpg
+
+ tweak -w 1.wcs -x 1.xy -r 1.rd -v
+
+ **/
+
 #include "starutil.h"
 #include "mathutil.h"
 #include "bl.h"
@@ -38,6 +51,7 @@
 #include "fitsioutils.h"
 #include "blind_wcs.h"
 #include "verify.h"
+#include "histogram2d.h"
 
 #include "plotstuff.h"
 #include "plotimage.h"
@@ -187,6 +201,83 @@ int main(int argc, char** args) {
 
 		// ??
 		// Look for index-field pairs that are (a) close together; and (b) close to CRPIX.
+
+		// Split the image into 3x3, 5x5 or so, and in each, look for a
+		// (small) rotation and log(scale), then (bigger) shift, using histogram
+		// cross-correlation.
+
+		// Are the rotations and scales really going to be big enough that this
+		// is required, or can we get away with doing shift first, then fine-tuning
+		// rotation and scale?
+
+		{
+			// NxN blocks
+			int NB = 3;
+			int b;
+			// HACK - use histogram2d machinery to split image into blocks.
+			histogram2d* blockhist = histogram2d_new_nbins(0, W, NB, 0, H, NB);
+			int* fieldi = malloc(Nfield * sizeof(int));
+			int* indexi = malloc(Nindex * sizeof(int));
+			// rotation bins
+			int NR = 100;
+			// scale bins (ie, log(radius) bins)
+			double minrad = 1.0;
+			double maxrad = 200.0;
+			int NS = 100;
+			histogram2d* rsfield = histogram2d_new_nbins(-M_PI, M_PI, NR,
+														 log(minrad), log(maxrad), NS);
+			histogram2d* rsindex = histogram2d_new_nbins(-M_PI, M_PI, NR,
+														 log(minrad), log(maxrad), NS);
+			histogram2d_set_y_edges(rsfield, HIST2D_DISCARD);
+			histogram2d_set_y_edges(rsindex, HIST2D_DISCARD);
+
+			for (b=0; b<(NB*NB); b++) {
+				int bin;
+				int NF, NI;
+				double dx, dy;
+				NF = NI = 0;
+				for (i=0; i<Nfield; i++) {
+					bin = histogram2d_add(blockhist, fieldpix[2*i], fieldpix[2*i+1]);
+					if (bin != b)
+						continue;
+					fieldi[NF] = i;
+					NF++;
+				}
+
+				for (i=0; i<Nindex; i++) {
+					bin = histogram2d_add(blockhist, indexpix[2*i], indexpix[2*i+1]);
+					if (bin != b)
+						continue;
+					indexi[NI] = i;
+					NI++;
+				}
+				logmsg("bin %i has %i field and %i index stars.\n", b, NF, NI);
+
+				logmsg("histogramming field rotation/scale\n");
+				for (i=0; i<NF; i++) {
+					for (j=0; j<i; j++) {
+						dx = fieldpix[2*fieldi[i]] - fieldpix[2*fieldi[j]];
+						dy = fieldpix[2*fieldi[i]+1] - fieldpix[2*fieldi[j]+1];
+						histogram2d_add(rsfield, atan2(dy, dx), log(sqrt(dx*dx + dy*dy)));
+					}
+				}
+				logmsg("histogramming index rotation/scale\n");
+				for (i=0; i<NI; i++) {
+					for (j=0; j<i; j++) {
+						dx = indexpix[2*indexi[i]] - fieldpix[2*indexi[j]];
+						dy = indexpix[2*indexi[i]+1] - fieldpix[2*indexi[j]+1];
+						histogram2d_add(rsindex, atan2(dy, dx), log(sqrt(dx*dx + dy*dy)));
+					}
+				}
+
+
+			}
+			histogram2d_free(rsfield);
+			histogram2d_free(rsindex);
+			free(fieldi);
+			free(indexi);
+			histogram2d_free(blockhist);
+		}
 
 		{
 			double* fieldsigma2s = malloc(Nfield * sizeof(double));
