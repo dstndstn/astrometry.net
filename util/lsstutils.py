7b72815aabfb34b8fb02ce22a1bdac81cb4d9e0f
Dustin Lang <dstndstn@gmail.com>
2010-06-25 21:43:34 +0000

    tabify
    git-svn-id: svn+ssh://astrometry.net/svn/trunk/src/astrometry@15282 03a3cea6-2c03-0410-ac9b-9271b1c66c29

--- util/lsstutils.py
+++ util/lsstutils.py
@@ -2,48 +2,48 @@ from lsst.afw.detection import SourceSet, Source
 
 # eg, from astrometry.util.pyfits_utils : fits_table() or text_table()
 def sourceset_from_table(t):
-    N = len(t)
-    ss = SourceSet()
-    for i in range(N):
-        s = Source()
-        ss.push_back(s)
-    for f in ["XAstrom", "XAstromErr", "YAstrom", "YAstromErr",
-              "PsfFlux", "ApFlux", "Ixx", "IxxErr", "Iyy",
-              "IyyErr", "Ixy", "IxyErr"]:
+	N = len(t)
+	ss = SourceSet()
+	for i in range(N):
+		s = Source()
+		ss.push_back(s)
+	for f in ["XAstrom", "XAstromErr", "YAstrom", "YAstromErr",
+			  "PsfFlux", "ApFlux", "Ixx", "IxxErr", "Iyy",
+			  "IyyErr", "Ixy", "IxyErr"]:
 		f = f.lower()
-        vals = t.getcolumn(f)
-        for s,v in zip(ss,vals):
-            func = getattr(s, "set" + f)
-            func(v)
-    return ss
+		vals = t.getcolumn(f)
+		for s,v in zip(ss,vals):
+			func = getattr(s, "set" + f)
+			func(v)
+	return ss
 	
 
 def sourceset_to_dict(ss):
-    d = dict()
-    for f in ["XAstrom", "XAstromErr", "YAstrom", "YAstromErr",
-              "PsfFlux", "ApFlux", "Ixx", "IxxErr", "Iyy",
-              "IyyErr", "Ixy", "IxyErr"]:
-        vals = []
-        for s in ss:
-            func = getattr(s, "get" + f)
-            vals.append(func())
-        d[f] = vals
-    return d
+	d = dict()
+	for f in ["XAstrom", "XAstromErr", "YAstrom", "YAstromErr",
+			  "PsfFlux", "ApFlux", "Ixx", "IxxErr", "Iyy",
+			  "IyyErr", "Ixy", "IxyErr"]:
+		vals = []
+		for s in ss:
+			func = getattr(s, "get" + f)
+			vals.append(func())
+		d[f] = vals
+	return d
 
 def sourceset_from_dict(d):
-    x = d['XAstrom']
-    N = len(x)
-    ss = SourceSet()
-    for i in range(N):
-        s = Source()
-        ss.push_back(s)
+	x = d['XAstrom']
+	N = len(x)
+	ss = SourceSet()
+	for i in range(N):
+		s = Source()
+		ss.push_back(s)
 
-    for f in ["XAstrom", "XAstromErr", "YAstrom", "YAstromErr",
-              "PsfFlux", "ApFlux", "Ixx", "IxxErr", "Iyy",
-              "IyyErr", "Ixy", "IxyErr"]:
-        vals = d[f]
-        for s,v in zip(ss,vals):
-            func = getattr(s, "set" + f)
-            func(v)
+	for f in ["XAstrom", "XAstromErr", "YAstrom", "YAstromErr",
+			  "PsfFlux", "ApFlux", "Ixx", "IxxErr", "Iyy",
+			  "IyyErr", "Ixy", "IxyErr"]:
+		vals = d[f]
+		for s,v in zip(ss,vals):
+			func = getattr(s, "set" + f)
+			func(v)
 
-    return ss
+	return ss
