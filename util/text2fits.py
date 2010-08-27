09a39a5b850c4cd4979d7a7c4873799c25de5074
Dustin Lang <dstndstn@gmail.com>
2010-08-27 13:45:52 +0000

    add PATH to ups setup
    git-svn-id: svn+ssh://astrometry.net/svn/trunk/src/astrometry@15990 03a3cea6-2c03-0410-ac9b-9271b1c66c29

--- util/text2fits.py
+++ util/text2fits.py
@@ -7,9 +7,9 @@ from astrometry.util.pyfits_utils import *
 if __name__ == '__main__':
 	p = OptionParser(usage='Usage: %prog [options] <input-text-file> <output-fits-table>')
 	p.add_option('-s', dest='separator', help='Separator character (default: whitespace)')
-	#p.add_option('-S', dest='skiplines', type='int', help='Skip this number of lines before the header')
+	p.add_option('-S', dest='skiplines', type='int', help='Skip this number of lines before the header')
 	p.add_option('-m', dest='maxcols', type='int', help='Trim each data row to this number of characters.')
-	p.set_defaults(separator=None, maxcols=None)
+	p.set_defaults(separator=None, maxcols=None, skiplines=0)
 	(opt,args) = p.parse_args()
 	if len(args) != 2:
 		p.print_help()
@@ -17,7 +17,8 @@ if __name__ == '__main__':
 	textfn = args[0]
 	fitsfn = args[1]
 	
-	t = text_table_fields(textfn, split=opt.separator, maxcols=opt.maxcols)
+	t = text_table_fields(textfn, split=opt.separator, maxcols=opt.maxcols,
+						  skiplines=opt.skiplines)
 	t.write_to(fitsfn)
 
 	
