78a653569dedb636e98c2082270d494eb9778a20
Dustin Lang <dstndstn@gmail.com>
2011-01-21 11:32:13 +0000

    misc
    git-svn-id: svn+ssh://astrometry.net/svn/trunk/src/astrometry@17217 03a3cea6-2c03-0410-ac9b-9271b1c66c29

--- sdss/test_dr7.py
+++ sdss/test_dr7.py
@@ -1,6 +1,12 @@
 import os
+from math import sqrt
+
+if __name__ == '__main__':
+	import matplotlib
+	matplotlib.use('Agg')
 
 import numpy as np
+import pylab as plt
 
 import astrometry.sdss
 from astrometry.sdss import DR7
@@ -34,3 +40,57 @@ if __name__ == '__main__':
 	ix,iy = asi.radec_to_pixel(ri, di)
 	print 'r', rx, ry
 	print 'i', ix, iy
+
+	tsobj = fits_table(os.path.join(testdata, 'cut-tsObj-002830-6-0-0398.fit'))
+	ra,dec = tsobj.ra, tsobj.dec
+	#X,Y = tsobj.colc[:,rband], tsobj.rowc[:,rband]
+	X,Y = tsobj.objc_colc, tsobj.objc_rowc
+	print 'ra,dec', ra.shape, dec.shape
+	print 'x', X.shape
+	#rmag = tsobj.psfcounts[:,rband]
+	#imag = tsobj.psfcounts[:,iband]
+	rmag = tsobj.counts_model[:,rband]
+	imag = tsobj.counts_model[:,iband]
+	# According to http://www.sdss.org/dr7/dm/flatFiles/tsField.html,
+	#   r,i,z fields, use this color:
+	color = (rmag - imag)
+	print 'color:', color.min(), color.max()
+
+	xx,yy = asr.radec_to_pixel(ra, dec, color)
+	print 'xx', xx.shape
+	rr,dd = asr.pixel_to_radec(X, Y, color)
+
+	print 'dxy', xx-X, yy-Y
+	#print 'dradec', ra-rr, dec-dd
+
+	print 'x RMS:', sqrt(np.mean((xx - X)**2))
+	print 'y RMS:', sqrt(np.mean((yy - Y)**2))
+	print 'RA RMS:', sqrt(np.mean((ra - rr)**2))
+	print 'Dec RMS:', sqrt(np.mean((dec - dd)**2))
+
+	#I = np.argmax((xx - X)**2 + (yy - Y)**2)
+	#print 'Biggest x,y deviant: row', I
+	#tsobj[I].about()
+	
+	plt.clf()
+	plt.plot(xx,yy, 'r.')
+	plt.plot(X, Y,  'bo', mfc='none')
+	plt.savefig('dxy.png')
+	
+	lo,hi = 1e-10, 10
+	rng = np.log10(lo), np.log10(hi)
+	plt.clf()
+	plt.hist(np.log10(np.clip(np.abs(xx - X), lo, hi)), range=rng)
+	plt.xlabel('log_10 ( x error )')
+	plt.savefig('dx.png')
+
+	plt.clf()
+	plt.hist(np.log10(np.clip(np.abs(yy - Y), lo, hi)), range=rng)
+	plt.xlabel('log_10 ( y error )')
+	plt.savefig('dy.png')
+
+	plt.clf()
+	plt.hist(np.log10(np.clip(np.sqrt((yy - Y)**2 + (xx - X)**2), lo, hi)),
+			 range=rng)
+	plt.xlabel('log_10 ( x,y error )')
+	plt.savefig('dr.png')
