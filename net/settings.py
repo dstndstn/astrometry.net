b0ccfac91e9ef991eaca37a1125fc48caa1fc4fa
Dustin Lang <dstndstn@gmail.com>
2011-06-06 16:35:27 +0000

    start phresh
    git-svn-id: svn+ssh://astrometry.net/svn/trunk/src/astrometry@18525 03a3cea6-2c03-0410-ac9b-9271b1c66c29

---
+++ net/settings.py
@@ -0,0 +1,210 @@
+import astrometry.net.secrets.django_db as secrets
+
+
+# FIXME
+BASEDIR = '/home/dstn/astrometry/src/'
+DATADIR = '/home/dstn/test/'
+LOG_DIR = BASEDIR + 'astrometry/net/log/'
+ANDIR = BASEDIR + 'astrometry/'
+UTIL_DIR = ANDIR + 'util/'
+BLIND_DIR = ANDIR + 'blind/'
+WEB_DIR = ANDIR + 'net/'
+JOB_QUEUE_DIR = DATADIR + 'job-queue/'
+
+SITE_NAME = 'edge'
+JOB_DIR = DATADIR + 'web-data/'
+#FIELD_DIR = '/home/gmaps/test/web-data/fields'
+FIELD_DIR = DATADIR + 'fields'
+
+BASE_URL = 'http://oven.cosmo.fas.nyu.edu:9000/'
+
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/job/newurl/'
+
+LOGFILE = LOG_DIR + 'django.log'
+PORTAL_LOGFILE = LOG_DIR + 'portal.log'
+VO_LOGFILE = LOG_DIR + 'vo.log'
+
+# Plotting
+HENRY_DRAPER_CAT = ANDIR + 'net/hd.fits'
+TYCHO_MKDT = ANDIR + 'net/tycho.mkdt.fits'
+TILERENDER = ANDIR + 'render/tilerender'
+SIPSO = ANDIR + 'util/_sip.so'
+CACHEDIR = DATADIR + 'tilecache/'
+RENDERCACHEDIR = DATADIR + 'rendercache/'
+TEMPDIR = '/tmp'
+
+SESSION_COOKIE_NAME = 'AstrometryTestSession'
+
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+    # ('Your Name', 'your_email@example.com'),
+)
+
+MANAGERS = ADMINS
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+		# Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'an-test',
+        'USER': secrets.DATABASE_USER,
+        'PASSWORD': secrets.DATABASE_PASSWORD,
+        'HOST': secrets.DATABASE_HOST,
+        'PORT': secrets.DATABASE_PORT,
+		}
+}
+
+# Local time zone for this installation. Choices can be found here:
+# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+# although not all choices may be available on all operating systems.
+# On Unix systems, a value of None will cause Django to use the same
+# timezone as the operating system.
+# If running in a Windows environment this must be set to the same as your
+# system time zone.
+TIME_ZONE = 'America/New York'
+
+# Language code for this installation. All choices can be found here:
+# http://www.i18nguy.com/unicode/language-identifiers.html
+LANGUAGE_CODE = 'en-us'
+
+SITE_ID = 1
+
+# If you set this to False, Django will make some optimizations so as not
+# to load the internationalization machinery.
+USE_I18N = True
+
+# If you set this to False, Django will not format dates, numbers and
+# calendars according to the current locale
+USE_L10N = True
+
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Example: "/home/media/media.lawrence.com/media/"
+#MEDIA_ROOT = ''
+# Absolute path to the directory that holds media.
+MEDIA_ROOT = WEB_DIR + 'media/'
+
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash.
+# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
+MEDIA_URL = ''
+
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT = ''
+
+# URL prefix for static files.
+# Example: "http://media.lawrence.com/static/"
+STATIC_URL = '/static/'
+
+# URL prefix for admin static files -- CSS, JavaScript and images.
+# Make sure to use a trailing slash.
+# Examples: "http://foo.com/static/admin/", "/static/admin/".
+ADMIN_MEDIA_PREFIX = '/static/admin/'
+
+# Additional locations of static files
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+)
+
+# List of finder classes that know how to find static files in
+# various locations.
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = 'd_&$%*@=ttb$qu047w0_35g=t@9+brymn)_si787g*52x_9e%n'
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+#     'django.template.loaders.eggs.Loader',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+)
+
+ROOT_URLCONF = 'urls'
+
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    WEB_DIR,
+)
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    # Uncomment the next line to enable the admin:
+    # 'django.contrib.admin',
+    # Uncomment the next line to enable admin documentation:
+    # 'django.contrib.admindocs',
+	'astrometry.net',
+)
+
+# A sample logging configuration. The only tangible logging
+# performed by this configuration is to send an email to
+# the site admins on every HTTP 500 error.
+# See http://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+	'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+		},
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+		},
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+		'null': {
+            'level':'DEBUG',
+            'class':'django.utils.log.NullHandler',
+		},
+        'console':{
+            'level':'DEBUG',
+            'class':'logging.StreamHandler',
+            'formatter': 'simple'
+		},
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True,
+		},
+        #'myproject.custom': {
+        #    'handlers': ['console', 'mail_admins'],
+        #    'level': 'INFO',
+        #    'filters': ['special']
+        #}
+    }
+}
---
+++ net/urls.py
@@ -0,0 +1,26 @@
+from django.conf.urls.defaults import patterns, include, url
+
+from astrometry.net import settings
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = (patterns('',
+
+						)
+			   )
+
+
+'''
+			   patterns('astrometry.net1.portal.api',
+						(r'^api/login', 'login'),
+						(r'^api/logout', 'logout'),
+						(r'^api/amiloggedin', 'amiloggedin'),
+						(r'^api/jobstatus', 'jobstatus'),
+						(r'^api/substatus', 'substatus'),
+						(r'^api/submit_url', 'submit_url'),
+						)
+			   )
+'''
+						
