59a5030c28fc1188be9da41db676d067a01de02a
Carlos Lalimarmo <clalimarmo@gmail.com>
2011-06-07 19:34:55 +0000

    finished models
    git-svn-id: svn+ssh://astrometry.net/svn/trunk/src/astrometry@18538 03a3cea6-2c03-0410-ac9b-9271b1c66c29

--- net/models.py
+++ net/models.py
@@ -18,29 +18,28 @@ class Tag(models.Model):
     text = models.CharField(max_length=4096)
     added_time = models.DateTimeField(auto_now=True) 
 
-class Job(models.Model):
-    calibration = models.ForeignKey(Calibration)
-    
-    STATUS_CHOICES = (('S', 'Success'), 
-              ('F', 'Failure'))    
-    
-    status = models.CharField(max_length=1, choices=STATUS_CHOICES)
-    error_message = models.CharField(max_length=256)
-
- 
 class Calibration(models.Model):
     # TAN WCS, straight from the quad match
-    raw_tan = models.ForeignKey(TanWCS, related_name='calibrations_raw', null=True)
+    raw_tan = models.ForeignKey('TanWCS', related_name='calibrations_raw', null=True)
     # TAN WCS, after tweaking
-    tweaked_tan = models.ForeignKey(TanWCS, related_name='calibrations_tweaked', null=True)
+    tweaked_tan = models.ForeignKey('TanWCS', related_name='calibrations_tweaked', null=True)
     # SIP
-    sip = models.ForeignKey(SipWCS, null=True)
+    sip = models.ForeignKey('SipWCS', null=True)
 
     # RA,Dec bounding box.
     ramin  = models.FloatField()
     ramax  = models.FloatField()
     decmin = models.FloatField()
     decmax = models.FloatField()
+
+class Job(models.Model):
+    calibration = models.ForeignKey('Calibration')
+    
+    STATUS_CHOICES = (('S', 'Success'), 
+              ('F', 'Failure'))    
+    
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES)
+    error_message = models.CharField(max_length=256)
 
 class UserImage(models.Model):
     image = models.ForeignKey(Image)
@@ -49,13 +48,12 @@ class UserImage(models.Model):
              ('pr', 'Private'))
 
     permission = models.CharField(max_length=2, choices=PERMISSION_CHOICES)
-    tags = models.ManyToManyField(Tag, related_name='images')
+    tags = models.ManyToManyField('Tag', related_name='images')
     description = models.CharField(max_length=1024)
     original_file_name = models.CharField(max_length=256)
-    submission = models.ForeignKey(Submission, related_name='user_images')
+    submission = models.ForeignKey('Submission', related_name='user_images')
 
 class Submission(models.Model):
-
     SCALEUNITS_CHOICES = (
         ('arcsecperpix', 'arcseconds per pixel'),
         ('arcminwidth' , 'width of the field (in arcminutes)'), 
@@ -67,35 +65,37 @@ class Submission(models.Model):
     SCALETYPE_CHOICES = (
         ('ul', 'lower and upper bounds'),
         ('ev', 'estimate and error bound'),
-        )
+    )
 
     PARITY_CHOICES = (
         (2, 'Try both simultaneously'),
         (0, 'Positive'),
         (1, 'Negative'),
-        )
+    )
 
     DATASRC_CHOICES = (
         ('url', 'URL'),
         ('file', 'File'),
-        )
+    )
 
     FILETYPE_CHOICES = (
         ('image', 'Image (jpeg, png, gif, tiff, raw, or FITS)'),
         ('fits', 'FITS table of source locations'),
         ('text', 'Text list of source locations'),
-        )
+    )
+
     ###
     disk_file = models.ForeignKey(DiskFile, related_name='submissions')
-    
-	datasrc = models.CharField(max_length=10, choices=DATASRC_CHOICES)
-
-	# image / fits / text
-	filetype = models.CharField(max_length=10, choices=FILETYPE_CHOICES)
-
-	url = models.URLField(blank=True, null=True)
-
-	parity = models.PositiveSmallIntegerField(choices=PARITY_CHOICES, default=2)
-    
+    data_src = models.CharField(max_length=10, choices=DATASRC_CHOICES)
+    # image / fits / text
+    file_type = models.CharField(max_length=10, choices=FILETYPE_CHOICES)
+    url = models.URLField(blank=True, null=True)
+    parity = models.PositiveSmallIntegerField(choices=PARITY_CHOICES, default=2)
     scale_units = models.CharField(max_length=20, choices=SCALEUNITS_CHOICES)
     scale_type = models.CharField(max_length=2, choices=SCALETYPE_CHOICES)
+    original_filename = models.CharField(max_length=256)
+
+class Album(models.Model):
+    description = models.CharField(max_length=1024)
+    user_images = models.ManyToManyField('UserImage', related_name='albums') 
+    tags = models.ManyToManyField('Tag', related_name='albums') 
