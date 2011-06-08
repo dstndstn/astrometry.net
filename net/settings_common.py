be817eb6456d451498e22d99d0ccf753d28dcffb
Carlos Lalimarmo <clalimarmo@gmail.com>
2011-06-08 15:16:19 +0000

    moved *.html into net/templates
    git-svn-id: svn+ssh://astrometry.net/svn/trunk/src/astrometry@18551 03a3cea6-2c03-0410-ac9b-9271b1c66c29

--- net/settings_common.py
+++ net/settings_common.py
@@ -151,7 +151,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    WEB_DIR,
+    os.path.join(WEB_DIR, 'templates') ,
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
