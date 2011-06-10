b52d0f4a5fd29e73f1c60629b20272f346064c10
Carlos Lalimarmo <clalimarmo@gmail.com>
2011-06-10 04:00:56 +0000

    added dict_pack method to ease loading tuples of dictionaries with repeated keys
    git-svn-id: svn+ssh://astrometry.net/svn/trunk/src/astrometry@18602 03a3cea6-2c03-0410-ac9b-9271b1c66c29

--- net/util.py
+++ net/util.py
@@ -1,3 +1,11 @@
+def dict_pack(struct_tuple, data_tuple):
+    pack = {}
+    for data in data_tuple:
+        index = 0
+        for key in struct_tuple:
+            pack.update({key:data[index]})
+            index += 1
+
 def choicify(choice_dict_list, database_value, human_readable_value):
     choice_list = []
     for dict in choice_dict_list:
