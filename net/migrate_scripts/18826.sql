0ad0d59ee6aa8841e382ed63c67ab16476f39891
nova alias <nova@astrometry.net>
2011-06-25 02:08:26 +0000

    added migrate_scripts directory, and script to migrate schema to r18826
    git-svn-id: svn+ssh://astrometry.net/svn/trunk/src/astrometry@18827 03a3cea6-2c03-0410-ac9b-9271b1c66c29

---
+++ net/migrate_scripts/18826.sql
@@ -0,0 +1,4 @@
+ALTER TABLE net_submission ADD COLUMN positional_error double precision;
+ALTER TABLE net_submission ADD COLUMN center_ra double precision;
+ALTER TABLE net_submission ADD COLUMN center_dec double precision;
+ALTER TABLE net_submission ADD COLUMN radius double precision;
