22ce369833d6b45130f126a776edf962752b17f8
Carlos Lalimarmo <clalimarmo@gmail.com>
2011-06-30 17:38:47 +0000

    resolved #968
    git-svn-id: svn+ssh://astrometry.net/svn/trunk/src/astrometry@18907 03a3cea6-2c03-0410-ac9b-9271b1c66c29

--- net/views/home.py
+++ net/views/home.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render_to_response, get_object_or_404, redirect
 from django.template import Context, RequestContext
+from astrometry.net.models import *
 
 def home(req):
-    context = {}
+    context = {
+        'images':UserImage.objects.all(),
+    }
     return render_to_response('home.html', context,
         context_instance = RequestContext(req))
 
