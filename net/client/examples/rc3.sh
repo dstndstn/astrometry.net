4e6356d87b3240bb954166cc567e85fa8373da40
David W. Hogg <dwhogg@gmail.com>
2011-07-30 13:10:49 +0000

    client: run on nova
    git-svn-id: svn+ssh://astrometry.net/svn/trunk/src/astrometry@19211 03a3cea6-2c03-0410-ac9b-9271b1c66c29

--- net/client/examples/rc3.sh
+++ net/client/examples/rc3.sh
@@ -1,9 +1,11 @@
 #/bin/bash
 #
 # calling sequence
-# ./rc3.sh apikey
+# ./rc3.sh apikey > ./script
+# bash ./script
 
-for file in $(ls ~/rc3/hard*.jpg)
+for file in $(ls ~/rc3/*hard.jpg)
 do
-    cmd = "python client.py --server http://supernova.astrometry.net/api/ --apikey $1 --upload $file"
+    cmd="python ../client.py --server http://nova.astrometry.net/api/ --apikey $1 --upload $file"
+    echo $cmd
 done
