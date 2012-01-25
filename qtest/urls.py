b42810cdb761893ad29a61f9871cbda4959d6095
Dustin Lang <dstndstn@gmail.com>
2012-01-25 21:46:47 +0000

    remove old crufty cruft
    git-svn-id: svn+ssh://astrometry.net/svn/trunk/src/astrometry@20184 03a3cea6-2c03-0410-ac9b-9271b1c66c29

--- qtest/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('',
-    # Example:
-    # (r'^qtest/', include('qtest.foo.urls')),
-
-    # Uncomment this for admin:
-#     (r'^admin/', include('django.contrib.admin.urls')),
-)
