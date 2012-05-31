1f1de5add69462701038120b4caf503642b6b744
nova alias <nova@astrometry.net>
2012-05-31 17:48:55 +0000

    union jobs too
    git-svn-id: svn+ssh://astrometry.net/svn/trunk/src/astrometry@20803 03a3cea6-2c03-0410-ac9b-9271b1c66c29

--- net/remake-data-dir.sh
+++ net/remake-data-dir.sh
@@ -4,21 +4,39 @@ set -u
 
 NOVADATA=/home/nova/nova/net/data
 STAGINGDATA=/home/nova/staging/net/data
-OVERLAY=/home/nova/staging/net/staging-overlay
+OVERLAYDATA=/home/nova/staging/net/staging-data-overlay
+
+NOVAJOBS=/home/nova/nova/net/jobs
+STAGINGJOBS=/home/nova/staging/net/jobs
+OVERLAYJOBS=/home/nova/staging/net/staging-jobs-overlay
+
+mkdir -p ${STAGINGDATA}
+mkdir -p ${STAGINGJOBS}
 
 # Unmount, if already mounted
 (grep -q "unionfs-fuse ${STAGINGDATA}" /etc/mtab &&
  echo Unmounting ${STAGINGDATA} &&
  fusermount -u ${STAGINGDATA}) || true
 
+(grep -q "unionfs-fuse ${STAGINGJOBS}" /etc/mtab &&
+ echo Unmounting ${STAGINGJOBS} &&
+ fusermount -u ${STAGINGJOBS}) || true
+
 # Clear/create the overlay area
-rm -Rf ${OVERLAY}
-mkdir ${OVERLAY}
+rm -Rf ${OVERLAYDATA}
+mkdir ${OVERLAYDATA}
+
+rm -Rf ${OVERLAYJOBS}
+mkdir ${OVERLAYJOBS}
 
 ## create the union filesystem:
 echo Mounting ${STAGINGDATA}
 unionfs-fuse -o cow,use_ino,suid,dev,nonempty,allow_other,default_permissions \
-    "${OVERLAY}=RW:${NOVADATA}=RO" "${STAGINGDATA}"
+    "${OVERLAYDATA}=RW:${NOVADATA}=RO" "${STAGINGDATA}"
+
+echo Mounting ${STAGINGJOBS}
+unionfs-fuse -o cow,use_ino,suid,dev,nonempty,allow_other,default_permissions \
+    "${OVERLAYJOBS}=RW:${NOVAJOBS}=RO" "${STAGINGJOBS}"
 
 #    -o max_files=10000
 #    -o allow_root       
