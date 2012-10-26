7d21b55f2de7cd2c34cd3cd985824178d382398d
Dustin Lang <dstndstn@gmail.com>
2012-10-26 19:59:35 +0000

    add 'stages' code
    git-svn-id: svn+ssh://astrometry.net/svn/trunk/src/astrometry@21597 03a3cea6-2c03-0410-ac9b-9271b1c66c29

---
+++ util/stages.py
@@ -0,0 +1,44 @@
+from astrometry.util.file import *
+
+class CallGlobal(object):
+	def __init__(self, pattern, *args, **kwargs):
+		self.pat = pattern
+		self.args = args
+		self.kwargs = kwargs
+	def __call__(self, stage, kwargs):
+		func = self.pat % stage
+		kwa = self.kwargs.copy()
+		kwa.update(kwargs)
+		return func(*self.args, **kwa)
+
+def runstage(stage, picklepat, stagefunc, force=[], prereqs={},
+			 **kwargs):
+	print 'Runstage', stage
+
+	pfn = picklepat % stage
+	if os.path.exists(pfn):
+		if stage in force:
+			print 'Ignoring pickle', pfn, 'and forcing stage', stage
+		else:
+			print 'Reading pickle', pfn
+			R = unpickle_from_file(pfn)
+			return R
+
+	if stage <= 0:
+		P = {}
+	else:
+		prereq = prereqs.get(stage, stage-1)
+
+		P = runstage(prereq, picklepat, stagefunc,
+					 force=force, prereqs=prereqs, **kwargs)
+	else:
+		P = {}
+
+	print 'Running stage', stage
+	R = stagefunc(stage, **P)
+	print 'Stage', stage, 'finished'
+
+	print 'Saving pickle', pfn
+	pickle_to_file(R, pfn)
+	print 'Saved', pfn
+	return R
