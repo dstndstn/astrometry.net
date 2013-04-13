1453b30a8b6b6a2ee77a2d0b973dc84750cfc8d4
Dustin Lang <dstndstn@gmail.com>
2013-04-13 02:02:38 +0000

    separate indexes for Lanczos and nearest-neighbour (to handle margin)
    git-svn-id: svn+ssh://astrometry.net/svn/trunk/src/astrometry@22577 03a3cea6-2c03-0410-ac9b-9271b1c66c29

--- util/resample.py
+++ util/resample.py
@@ -1,11 +1,15 @@
+import numpy as np
 import scipy.interpolate as interp
+from miscutils import lanczos_filter
 
-def resample_with_wcs(targetwcs, wcs, Limages, Lorder):
+def resample_with_wcs(targetwcs, wcs, Limages, L):
 	'''
 	Returns (Yo,Xo, Yi,Xi, ims)
 
 	Limages: list of images to Lanczos-interpolate at the given Lanczos order.
 	If empty, just returns nearest-neighbour indices.
+
+	L: int, lanczos order
 
 	Use the results like:
 
@@ -16,8 +20,14 @@ def resample_with_wcs(targetwcs, wcs, Limages, Lorder):
 	'''
 	# Adapted from detection/sdss-demo.py
 
-	H,W = targetwcs.imageh, targetwcs.imagew
-	h,w = wcs.imageh, wcs.imagew
+	H,W = int(targetwcs.imageh), int(targetwcs.imagew)
+	h,w = int(wcs.imageh), int(wcs.imagew)
+
+	for im in Limages:
+		assert(im.shape == (h,w))
+
+	print 'Target size', W, H
+	print 'Input size', w, h
 	
 	# First find the approximate bbox of the input image in
 	# the target image so that we don't ask for way too
@@ -26,12 +36,12 @@ def resample_with_wcs(targetwcs, wcs, Limages, Lorder):
 	for x,y in [(0,0), (w-1,0), (w-1,h-1), (0, h-1)]:
 		#rd = wcs.pixelToPosition(x,y)
 		#XY.append(targetwcs.positionToPixel(rd))
-		ra,dec = wcs.pixelxy2radec(float(x), float(y))
+		ra,dec = wcs.pixelxy2radec(float(x + 1), float(y + 1))
 		ok,x,y = targetwcs.radec2pixelxy(ra, dec)
-		XY.append((x,y))
+		XY.append((x - 1,y - 1))
 	XY = np.array(XY)
 	# Now we build a spline that maps "target" pixels to "input" pixels
-	# spline inputs:
+	# spline inputs: pixel coords in the 'output' image
 	x0,y0 = np.round(XY.min(axis=0)).astype(int)
 	x1,y1 = np.round(XY.max(axis=0)).astype(int)
 	margin = 20
@@ -48,11 +58,11 @@ def resample_with_wcs(targetwcs, wcs, Limages, Lorder):
 		for x in xx:
 			#rd = targetwcs.pixelToPosition(x,y)
 			#XYo.append(wcs.positionToPixel(rd))
-			ra,dec = targetwcs.pixelxy2radec(float(x), float(y))
-			ok,x,y = wcs.radec2pixelxy(ra,dec)
-			XYo.append((x,y))
+			ra,dec = targetwcs.pixelxy2radec(float(x + 1), float(y + 1))
+			ok,xw,yw = wcs.radec2pixelxy(ra,dec)
+			XYo.append((xw - 1, yw - 1))
 	XYo = np.array(XYo)
-	# spline outputs -- pixel coords in the 'detmap'
+	# spline outputs -- pixel coords in the 'input' image
 	Xo = XYo[:,0].reshape(len(yy), len(xx))
 	Yo = XYo[:,1].reshape(len(yy), len(xx))
 
@@ -60,38 +70,58 @@ def resample_with_wcs(targetwcs, wcs, Limages, Lorder):
 	yspline = interp.RectBivariateSpline(xx, yy, Yo.T)
 
 	# Now, build the full pixel grid we want to interpolate...
-	xx = np.arange(max(0, x0-margin), min(W, x1+margin+1))
-	yy = np.arange(max(0, y0-margin), min(H, y1+margin+1))
-	# And run the interpolator
-	Xi = xspline(xx,yy).T
-	Yi = yspline(xx,yy).T
+	ixo = np.arange(max(0, x0-margin), min(W, x1+margin+1), dtype=int)
+	iyo = np.arange(max(0, y0-margin), min(H, y1+margin+1), dtype=int)
 
+	# And run the interpolator.  [xy]spline() does a meshgrid-like broadcast,
+	# so fxi,fyi 
+	fxi = xspline(ixo, iyo).T
+	fyi = yspline(ixo, iyo).T
+
+	ixi = np.round(fxi).astype(int)
+	iyi = np.round(fyi).astype(int)
+
+	# Keep only in-bounds pixels.
+	I = np.flatnonzero((ixi >= 0) * (iyi >= 0) * (ixi < w) * (iyi < h))
+
+	fxi = fxi[I]
+	fyi = fyi[I]
+	ixi = ixi[I]
+	iyi = iyi[I]
+
+	assert(np.all(ixi >= 0))
+	assert(np.all(iyi >= 0))
+	assert(np.all(ixi < w))
+	assert(np.all(iyi < h))
+
+	# Keep only in-bounds pixels... with a margin
 	if len(Limages):
 		m = L+1
-		# Keep only in-bounds pixels
-		I = np.flatnonzero((Xi >= m) * (Xi <= w-1-m) * (Yi >= m) * (Yi <= h-1-m))
-		# Center of interpolation
-		xi = Xi.flat[I]
-		yi = Yi.flat[I]
-		Xi = np.round(xi).astype(int)
-		Yi = np.round(yi).astype(int)
-		dx = Xi - xi
-		dy = Yi - yi
+		IL = np.flatnonzero((ixi >= m) * (ixi < w-m) * (iyi >= m) * (iyi < h-m))
 
-		Xo,Yo = xx[I%(len(xx))], yy[I/(len(xx))]
+		ilxo,ilyo = np.unravel_index(IL, fxi.shape)
+		ilxi,ilyi = ixi[IL], iyi[IL]
 
-		assert(np.all(Xi >= 0))
-		assert(np.all(Yi >= 0))
-		assert(np.all(Xo >= 0))
-		assert(np.all(Yo >= 0))
-		assert(np.all(Xi < w))
-		assert(np.all(Yi < h))
-		assert(np.all(Xo < W))
-		assert(np.all(Yo < H))
+		assert(np.all(ilxo >= 0))
+		assert(np.all(ilyo >= 0))
+		assert(np.all(ilxo < W))
+		assert(np.all(ilyo < H))
+
+		assert(np.all(ilxi >= 0))
+		assert(np.all(ilyi >= 0))
+		assert(np.all(ilxi < w))
+		assert(np.all(ilyi < h))
+
+		fxi -= ixi
+		fyi -= iyi
+		dx = fxi
+		dy = fyi
+		del fxi
+		del fyi
 
 		# Lanczos interpolation.
 		# number of pixels
-		nn = len(Yo)
+		nn = len(ilxo)
 		NL = 2*L+1
 
 		# We interpolate all the pixels at once.
@@ -108,7 +138,7 @@ def resample_with_wcs(targetwcs, wcs, Limages, Lorder):
 			for ox in off:
 				fx = lanczos_filter(L, ox + dx)
 				for lacc,im in zip(laccs, Limages):
-					lacc   += fx * fy * im[Yi + oy, Xi + ox]
+					lacc += fx * fy * im[ilyi + oy, ilxi + ox]
 				fsum += fx*fy
 		for lacc in laccs:
 			lacc /= fsum
@@ -118,4 +148,4 @@ def resample_with_wcs(targetwcs, wcs, Limages, Lorder):
 	else:
 		rims = []
 
-	return (Yo, Xo, Yi, Xi, rims)
+	return (ixo,iyo, ixi,iyi, ilxo,ilyo, ilxo,ilyo, rims)
