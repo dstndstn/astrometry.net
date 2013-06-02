d84af12680af01a20fa99f6696e72eeb7828425e
Dustin Lang <dstndstn@gmail.com>
2013-06-02 14:05:27 +0000

    sdss setup.py: create dependency-checking distutils builder, use it
    git-svn-id: svn+ssh://astrometry.net/svn/trunk/src/astrometry@22903 03a3cea6-2c03-0410-ac9b-9271b1c66c29

--- sdss/setup.py
+++ sdss/setup.py
@@ -1,11 +1,15 @@
 from distutils.core import setup, Extension
+import os
+import sys
 
-setup(name='C utils for Astrometry.net SDSS routines',
+sys.path.append(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'util'))
+from an_build_ext import an_build_ext
+
+setup(cmdclass={'build_ext': an_build_ext},
+	  name='C utils for Astrometry.net SDSS routines',
 	  version = '1.0',
 	  author = 'Lang & Hogg',
-	  author_email = 'dstn@astro.princeton.edu',
+	  author_email = 'dstn@cmu.edu',
 	  url = 'http://astrometry.net',
 	  ext_modules = [Extension('_cutils', ['cutils.i'])],
-	  #py_modules = ['cutils'],
-	  #swig_opts = ['-modern'],
-	  )
+	)
