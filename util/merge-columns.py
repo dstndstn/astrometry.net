c4794f510b3d7d1fb270f0712a3fb6055f145011
Dustin Lang <dstndstn@gmail.com>
2014-02-27 12:47:23 +0000

    add script to merge columns in python
    git-svn-id: svn+ssh://astrometry.net/svn/trunk/src/astrometry@24565 03a3cea6-2c03-0410-ac9b-9271b1c66c29

---
+++ util/merge-columns.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python
+
+from astrometry.util.fits import *
+import sys
+
+if __name__ == '__main__':
+    import optparse
+
+    parser = optparse.OptionParser(prog='[input files] [output file]')
+    opt,args = parser.parse_args()
+
+    if len(args) < 2:
+        parser.print_usage()
+        sys.exit(-1)
+
+    infns = args[:-1]
+    outfn = args[-1]
+
+    print 'Writing to output file', outfn
+    T = None
+    for fn in infns:
+        print 'Reading input', fn
+        Ti = fits_table(fn)
+        if T is None:
+            T = Ti
+        else:
+            T.add_columns_from(Ti)
+    T.writeto(outfn)
+    
+
+    
+    
