7b9732d31ae909dfb11c2c5a438e71c50819f759
Maik Riechert <maik.riechert@arcor.de>
2015-02-09 21:41:15 +0100

    nicer graphs + new layered graph

--- crowfood/create_graphs.sh
+++ crowfood/create_graphs.sh
@@ -3,3 +3,4 @@
 make clean
 make astrometry_grouped_big.pdf DRAW=dot
 make astrometry_grouped.pdf DRAW=circo
+make astrometry_layered.pdf DRAW=dot CLUSTER=cfood-cluster_regexp GRAPH_FLAGS="--shape box"
