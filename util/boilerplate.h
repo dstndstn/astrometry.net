58bae6f56270d3ca79736abb1328121fc6b840fc
Dustin Lang <dstndstn@gmail.com>
2015-03-11 07:12:47 -0400

    replace spaces by underscores in AN_GIT_DATE; use single-double quotes for command-line args

--- util/boilerplate.h
+++ util/boilerplate.h
@@ -26,7 +26,7 @@
 do {								             \
     fprintf(fid, "This program is part of the Astrometry.net suite.\n");     \
     fprintf(fid, "For details, visit http://astrometry.net.\n");	     \
-    fprintf(fid, "Git URL %s\n", AN_GIT_URL);				     \
+    fprintf(fid, "Git URL %s\n", AN_GIT_URL);                      \
     fprintf(fid, "Revision %s, date %s.\n", AN_GIT_REVISION, AN_GIT_DATE); \
 } while (0)
 
@@ -34,9 +34,9 @@ do {								             \
 do {                                                                             \
     fits_add_long_history(hdr, "Created by the Astrometry.net suite.");          \
     fits_add_long_history(hdr, "For more details, see http://astrometry.net.");  \
-    fits_add_long_history(hdr, "Git URL %s", AN_GIT_URL);		         \
-    fits_add_long_history(hdr, "Git revision %s", AN_GIT_REVISION);	         \
-    fits_add_long_history(hdr, "Git date %s", AN_GIT_DATE);		         \
+    fits_add_long_history(hdr, "Git URL %s", AN_GIT_URL);          \
+    fits_add_long_history(hdr, "Git revision %s", AN_GIT_REVISION); \
+    fits_add_long_history(hdr, "Git date %s", AN_GIT_DATE);        \
 } while (0)
 
 #endif
