be3b57e3004c36d146037526572b8fa3cd8d4dea
Miguel de Val-Borro <miguel@archlinux.net>
2016-10-02 18:36:20 -0400

    Fix string processing in parsing function for JPL ephemeris

--- util/jpl.c
+++ util/jpl.c
@@ -15,17 +15,20 @@ typedef struct orbital_elements orbital_elements_t;
 
 
 bl* jpl_parse_orbital_elements(const char* str, bl* lst) {
-	double jd;
+	double mjd;
 	double a, e, I, Omega, pomega, M;
 	double QR, Tp, N, TA, AD, PR;
 	char adstr[5];
 	char datestr[12];
 	char timestr[14];
 	char tzstr[5];
-	sscanf(s, "%lf = %4s %11s %13s %4s EC= %lf QR= %lf IN=%lf"
+	sscanf(str, "%lf = %4s %11s %13s %4s EC= %lf QR= %lf IN=%lf"
 		   " OM= %lf W = %lf Tp= %lf N = %lf MA= %lf TA= %lf"
 		   " A = %lf AD= %lf PR= %lf",
-		   &jd, adstr, datestr, timestr, tzstr, &e, &QR, &I,
+		   &mjd, adstr, datestr, timestr, tzstr, &e, &QR, &I,
 		   &Omega, &pomega, &Tp, &N, &M, &TA, &a, &AD, &PR);
+	lst = bl_new(3, sizeof(mjd));
+	bl_append(lst, &mjd);
+	bl_append(lst, &e);
 }
 
