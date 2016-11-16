419a812330c6046937d0343cbead595c6ffde885
Dustin Lang <dstndstn@gmail.com>
2016-11-16 12:33:30 -0500

    fix source extractor red-green plots

--- net/views/image.py
+++ net/views/image.py
@@ -408,6 +408,7 @@ def sdss_image(req, calid=None, size='full'):
 def red_green_image(req, job_id=None, size='full'):
     job = get_object_or_404(Job, pk=job_id)
     ui = job.user_image
+    sub = ui.submission
     img = ui.image
     if size == 'display':
         scale = float(img.get_display_image().width)/img.width
@@ -435,6 +436,9 @@ def red_green_image(req, job_id=None, size='full'):
         pimg.format = PLOTSTUFF_FORMAT_PPM
         plot.color = 'white'
         plot.alpha = 1.
+        if sub.use_sextractor:
+            xy.xcol = 'X_IMAGE'
+            xy.ycol = 'Y_IMAGE'
         plot.plot('image')
 
         # plot red
