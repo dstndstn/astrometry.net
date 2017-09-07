f11cb4773ed709bdb4923b9a0728b97b47d957c0
Dustin Lang <dstndstn@gmail.com>
2017-09-07 16:35:36 -0400

    don't actually need pngmath?

--- doc/conf.py
+++ doc/conf.py
@@ -25,7 +25,8 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.todo', 'sphinx.ext.pngmath',
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.todo',
+              #'sphinx.ext.pngmath',
               #'sphinx.ext.jsmath',
               'sphinx.ext.viewcode', 'numpydoc']
 
