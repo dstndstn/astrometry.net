2d08d4156b9cb07b03af031a491002d56ecf3b6d
Dustin Lang <dstndstn@gmail.com>
2019-02-14 16:57:02 -0500

    ugh python3.7 multiprocessing

--- util/fits.py
+++ util/fits.py
@@ -460,7 +460,7 @@ class tabledata(object):
             except Exception:
                 print('exception appending element "%s"' % name)
                 raise
-                
+
     def write_to(self, fn, columns=None, header='default', primheader=None,
                  use_fitsio=True, append=False, append_to_hdu=None,
                  fits_object=None,
