da4cc8f67de1b9867fdf61b86940d7b84da1f3b6
Will Silberman <wsilberm@google.com>
2019-04-04 18:06:53 -0700

    Simplifications suggested by Dustin

--- doc/install_astrometry_on_linux.sh
+++ doc/install_astrometry_on_linux.sh
@@ -1,30 +1,15 @@
 #!/bin/sh
-# TODO: a few of the commands require ‘yes/no’ answers, how to avoid this?
 
 WORKSPACE_DIR="/tmp"
 
-# basic linux install/build libs
+# basic linux install/build libs, python, and other support
 cd $WORKSPACE_DIR && \
-apt install make && \
+apt install -y make && \
 apt-get update && \
-apt-get install build-essential && \
-
-# python and other support
-apt-get install python python-pip netpbm zlib1g-dev libcairo2-dev libjpeg-dev && \
+apt-get install -y build-essential python python-pip netpbm zlib1g-dev libcairo2-dev libjpeg-dev libcfitsio-dev && \
 pip install numpy scipy pyfits && \
 
-# cfitsio
-cd $WORKSPACE_DIR && \
-rm -f cfitsio_latest.tar.gz* && \
-wget https://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio_latest.tar.gz && \
-tar xvzf cfitsio_latest.tar.gz && \
-cd cfitsio && \
-./configure && \
-make && \
-make install && \
-
 # astrometry
-cd $WORKSPACE_DIR && \
 rm -f astrometry.net-latest.tar.gz* && \
 wget http://astrometry.net/downloads/astrometry.net-latest.tar.gz && \
 tar xvzf astrometry.net-latest.tar.gz && \
@@ -36,4 +21,5 @@ make CFITS_INC="-I$WORKSPACE_DIR/cfitsio/include" CFITS_LIB="-L$WORKSPACE_DIR/cf
 
 # download and install index files
 rm -rf /usr/local/astrometry/data/* && \
-wget -r -nd -np -P /usr/local/astrometry/data/ "data.astrometry.net/4200/"
+wget -r -nd -np -P /usr/local/astrometry/data/ "data.astrometry.net/4100/" && \
+wget -r -nd -np -P /usr/local/astrometry/data/ "data.astrometry.net/5000/"
