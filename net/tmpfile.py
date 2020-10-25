66175ecfec666c6d5f5e8848b1a6899f9b1bf34c
nova @ broiler <dstndstn@gmail.com>
2020-10-25 17:15:12 +0000

    fix typo

--- net/tmpfile.py
+++ net/tmpfile.py
@@ -6,6 +6,6 @@ from astrometry.net.settings import TEMPDIR
 def get_temp_file(suffix='', tempfiles=None):
     f,fn = tempfile.mkstemp(dir=TEMPDIR, suffix=suffix)
     os.close(f)
-    if tempfiles is nott None:
+    if tempfiles is not None:
         tempfiles.append(fn)
     return fn
