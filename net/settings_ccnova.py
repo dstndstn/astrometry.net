a052c09a2050e6aca15523e6e8b490fdd134aaaa
nova <nova@nova.arbutus>
2022-05-16 15:35:09 +0000

    ccnova config

--- net/settings_ccnova.py
+++ net/settings_ccnova.py
@@ -1,7 +1,9 @@
 from __future__ import absolute_import
-sitename = 'ccnova'
+sitename = 'nova'
 
 from astrometry.net.settings_common import *
+
+DEBUG = False
 
 HENRY_DRAPER_CAT = '/data/nova/hd.fits'
 TYCHO2_KD = '/data/nova/tycho2.kd'
@@ -13,7 +15,7 @@ INSTALLED_APPS += SOCIAL_INSTALLED_APPS
 AUTHENTICATION_BACKENDS = SOCIAL_AUTH_BACKENDS + AUTHENTICATION_BACKENDS
 TEMPLATES[0]['OPTIONS']['context_processors'].extend(SOCIAL_TEMPLATE_CONTEXT_PROCESSORS)
 
-sitename = 'ccnova'
+sitename = 'nova'
 
 TEMPDIR = '/data/tmp'
 DATABASES['default']['NAME'] = 'an-nova'
