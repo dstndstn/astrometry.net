e6db3afe8f639c9ea6a36101e9c6703e705c3ecd
Dustin Lang <dstndstn@gmail.com>
2022-06-24 06:01:00 -0700

    tempfile middleware: handle streaming responses

--- net/tempfile_middleware.py
+++ net/tempfile_middleware.py
@@ -4,33 +4,41 @@ import shutil
 from astrometry.net.log import logmsg
 
 class TempfileMiddleware:
+    # One-time configuration and initialization (one time for the whole app)
     def __init__(self, get_response):
         self.get_response = get_response
-        # One-time configuration and initialization.
 
     def __call__(self, request):
-        # Code to be executed for each request before
-        # the view (and later middleware) are called.
+        # Called for each request
         request.tempfiles = []
         request.tempdirs = []
 
         response = self.get_response(request)
 
-        # Code to be executed for each request/response after
-        # the view is called.
-        for dirnm in request.tempdirs:
-            if os.path.exists(dirnm):
-                try:
-                    shutil.rmtree(dirnm)
-                except OSError as e:
-                    logmsg('Failed to delete temp dir', dirnm, ':', e)
-        for fn in request.tempfiles:
-            if os.path.exists(fn):
-                try:
-                    os.remove(fn)
-                except OSError as e:
-                    logmsg('Failed to delete temp file', fn, ':', e)
+        if response.streaming:
+            def wrap_streaming_content(content, request):
+                for chunk in content:
+                    yield chunk
+                #print('Sent all content')
+                delete_tempfiles_for_request(request)
+            #print('Streaming response')
+            response.streaming_content = wrap_streaming_content(response.streaming_content,
+                                                                request)
+            return response
+        else:
+            delete_tempfiles_for_request(request)
+            return response
 
-        return response
-
-    
+def delete_tempfiles_for_request(request):
+    for dirnm in request.tempdirs:
+        if os.path.exists(dirnm):
+            try:
+                shutil.rmtree(dirnm)
+            except OSError as e:
+                logmsg('Failed to delete temp dir', dirnm, ':', e)
+    for fn in request.tempfiles:
+        if os.path.exists(fn):
+            try:
+                os.remove(fn)
+            except OSError as e:
+                logmsg('Failed to delete temp file', fn, ':', e)
