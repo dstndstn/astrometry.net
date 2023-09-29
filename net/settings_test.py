57e9a0626a80353b9eef590a2959b3ceee72d1a1
Yan Wang <grapeot@outlook.com>
2023-09-28 18:24:32 -0700

    Update the Dockerfiles

--- net/settings_test.py
+++ net/settings_test.py
@@ -4,6 +4,8 @@ from astrometry.net.settings_common import *
 
 ENABLE_SOCIAL = False
 
+# Since this settings file is only for testing, disable the host restriction here to ensure a smooth deployment
+ALLOWED_HOSTS = ['*']
 DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
 DATABASES['default']['NAME'] = 'django.sqlite3'
 
